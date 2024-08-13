b4ec55c894610ae8e0d6b74201c630e13395ec1f
mt3hr <38493486+mt3hr@users.noreply.github.com>
2024-08-13 10:43:44 +0900

    clean

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,51 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import login_page from '../pages/login-page.vue'
+import kftl_page from '../pages/kftl-page.vue'
+import mi_page from '../pages/mi-page.vue'
+import rykv_page from '../pages/rykv-page.vue'
+import saihate_page from '../pages/saihate-page.vue'
+import set_new_password_page from '../pages/set-new-password-page.vue'
+import shared_mi_page from '../pages/shared-mi-page.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'login',
+      component: login_page,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/kftl',
+      name: 'kftl',
+      component: kftl_page,
+    },
+    {
+      path: '/mi',
+      name: 'mi',
+      component: mi_page,
+    },
+    {
+      path: '/rykv',
+      name: 'rykv',
+      component: rykv_page,
+    },
+    {
+      path: '/saihate',
+      name: 'saihate',
+      component: saihate_page,
+    },
+    {
+      path: '/',
+      name: 'set_new_password',
+      component: set_new_password_page,
+    },
+    {
+      path: '/shared_mi',
+      name: 'shared_mi',
+      component: shared_mi_page,
+    },
   ]
 })
 
