3158f1463b8e9b1e7b84e31b8992e94412bd81c7
mt3hr <38493486+mt3hr@users.noreply.github.com>
2024-08-18 13:16:07 +0900

    implements gpsLogRepository

--- src/app/gkill/dbo/reps/gps_log_repositories.go
+++ src/app/gkill/dbo/reps/gps_log_repositories.go
@@ -3,50 +3,158 @@ package reps
 
 import (
 	"context"
+	"fmt"
+	"sort"
+	"sync"
 	"time"
 )
 
 // ˄
 
-type GPSLogRepositories struct {
+type GPSLogRepositories []GPSLogRepository
+
+func (g GPSLogRepositories) GetAllGPSLogs(ctx context.Context) ([]*GPSLog, error) {
 	// ˅
+	gpsLogs := []*GPSLog{}
+	existErr := false
+	var err error
+	wg := &sync.WaitGroup{}
+	ch := make(chan []*GPSLog, len(g))
+	errch := make(chan error, len(g))
+	defer close(ch)
+	defer close(errch)
 
-	// ˄
+	// 並列処理
+	for _, rep := range g {
+		wg.Add(1)
+		rep := rep
+		go func(rep GPSLogRepository) {
+			defer wg.Done()
+			matchGPSLogsInRep, err := rep.GetAllGPSLogs(ctx)
+			if err != nil {
+				errch <- err
+				return
+			}
+			ch <- matchGPSLogsInRep
+		}(rep)
+	}
+	wg.Wait()
 
-	gpsLogRepositories []GPSLogRepository
+	// エラー集約
+errloop:
+	for {
+		select {
+		case e := <-errch:
+			err = fmt.Errorf("error at find gpsLogs: %w", e)
+			existErr = true
+		default:
+			break errloop
+		}
+	}
+	if existErr {
+		return nil, err
+	}
 
-	// ˅
+	// GPSLogs集約。UpdateTimeが最新のものを収める
+loop:
+	for {
+		select {
+		case matchGPSLogsInRep := <-ch:
+			if matchGPSLogsInRep == nil {
+				continue loop
+			}
+			gpsLogs = append(gpsLogs, matchGPSLogsInRep...)
+		default:
+			break loop
+		}
+	}
 
+	sort.Slice(gpsLogs, func(i, j int) bool {
+		return gpsLogs[i].RelatedTime.After(gpsLogs[j].RelatedTime)
+	})
+	return gpsLogs, nil
 	// ˄
 }
 
-func (g *GPSLogRepositories) GetAllGPSLogs(ctx context.Context) ([]*GPSLog, error) {
+func (g GPSLogRepositories) GetGPSLogs(ctx context.Context, startTime time.Time, endTime time.Time) ([]*GPSLog, error) {
 	// ˅
-	panic("notImplements")
+	gpsLogs := []*GPSLog{}
+	existErr := false
+	var err error
+	wg := &sync.WaitGroup{}
+	ch := make(chan []*GPSLog, len(g))
+	errch := make(chan error, len(g))
+	defer close(ch)
+	defer close(errch)
+
+	// 並列処理
+	for _, rep := range g {
+		wg.Add(1)
+		rep := rep
+		go func(rep GPSLogRepository) {
+			defer wg.Done()
+			matchGPSLogsInRep, err := rep.GetGPSLogs(ctx, startTime, endTime)
+			if err != nil {
+				errch <- err
+				return
+			}
+			ch <- matchGPSLogsInRep
+		}(rep)
+	}
+	wg.Wait()
+
+	// エラー集約
+errloop:
+	for {
+		select {
+		case e := <-errch:
+			err = fmt.Errorf("error at find gpsLogs: %w", e)
+			existErr = true
+		default:
+			break errloop
+		}
+	}
+	if existErr {
+		return nil, err
+	}
+
+	// GPSLogs集約。UpdateTimeが最新のものを収める
+loop:
+	for {
+		select {
+		case matchGPSLogsInRep := <-ch:
+			if matchGPSLogsInRep == nil {
+				continue loop
+			}
+			gpsLogs = append(gpsLogs, matchGPSLogsInRep...)
+		default:
+			break loop
+		}
+	}
+
+	sort.Slice(gpsLogs, func(i, j int) bool {
+		return gpsLogs[i].RelatedTime.After(gpsLogs[j].RelatedTime)
+	})
+	return gpsLogs, nil
 	// ˄
 }
 
-func (g *GPSLogRepositories) GetGPSLogs(ctx context.Context, startTime time.Time, endTime time.Time) ([]*GPSLog, error) {
+func (g GPSLogRepositories) GetPath(ctx context.Context, id string) (string, error) {
 	// ˅
-	panic("notImplements")
+	err := fmt.Errorf("not implements GPSLogReps.GetPath")
+	return "", err
 	// ˄
 }
 
-func (g *GPSLogRepositories) GetPath(ctx context.Context, id string) (string, error) {
+func (g GPSLogRepositories) GetRepName(ctx context.Context) (string, error) {
 	// ˅
-	panic("notImplements")
+	return "GPSLogReps", nil
 	// ˄
 }
 
-func (g *GPSLogRepositories) GetRepName(ctx context.Context) (string, error) {
+func (g GPSLogRepositories) UpdateCache(ctx context.Context) error {
 	// ˅
-	panic("notImplements")
-	// ˄
-}
-
-func (g *GPSLogRepositories) UpdateCache(ctx context.Context) error {
-	// ˅
-	panic("notImplements")
+	return nil
 	// ˄
 }
 
