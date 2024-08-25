7450c856587c325c63aa6cf6e4a10afd1f667c35
mt3hr <38493486+mt3hr@users.noreply.github.com>
2024-08-25 10:41:43 +0900

    implements Repositories, GkillDAOManager

--- src/app/gkill/dao/reps/repositories.go
+++ src/app/gkill/dao/reps/repositories.go
@@ -1,61 +1,339 @@
 // ˅
 package reps
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"sort"
+	"sync"
+
+	"github.com/mt3hr/gkill/src/app/gkill/dao/sqlite3impl"
+)
 
 // ˄
 
-type Repositories struct {
+type Repositories []Repository
+
+func (r Repositories) FindKyous(ctx context.Context, queryJSON string) ([]*Kyou, error) {
 	// ˅
+	matchKyous := map[string]*Kyou{}
+	existErr := false
+	var err error
+	wg := &sync.WaitGroup{}
+	ch := make(chan []*Kyou, len(r))
+	errch := make(chan error, len(r))
+	defer close(ch)
+	defer close(errch)
 
-	// ˄
+	// 並列処理
+	for _, rep := range r {
+		wg.Add(1)
+		rep := rep
+		go func(rep Repository) {
+			defer wg.Done()
+			matchKyousInRep, err := rep.FindKyous(ctx, queryJSON)
+			if err != nil {
+				errch <- err
+				return
+			}
+			ch <- matchKyousInRep
+		}(rep)
+	}
+	wg.Wait()
 
-	repositories []Repository
+	// エラー集約
+errloop:
+	for {
+		select {
+		case e := <-errch:
+			err = fmt.Errorf("error at find kyous: %w", e)
+			existErr = true
+		default:
+			break errloop
+		}
+	}
+	if existErr {
+		return nil, err
+	}
 
-	// ˅
+	// Kyou集約。UpdateTimeが最新のものを収める
+loop:
+	for {
+		select {
+		case matchKyousInRep := <-ch:
+			if matchKyousInRep == nil {
+				continue loop
+			}
+			for _, kyou := range matchKyousInRep {
+				if existKyou, exist := matchKyous[kyou.ID]; exist {
+					if kyou.UpdateTime.Before(existKyou.UpdateTime) {
+						matchKyous[kyou.ID] = kyou
+					}
+				} else {
+					matchKyous[kyou.ID] = kyou
+				}
+			}
+		default:
+			break loop
+		}
+	}
 
+	matchKyousList := []*Kyou{}
+	for _, kyou := range matchKyous {
+		if kyou == nil {
+			continue
+		}
+		matchKyousList = append(matchKyousList, kyou)
+	}
+
+	sort.Slice(matchKyousList, func(i, j int) bool {
+		return matchKyousList[i].RelatedTime.After(matchKyousList[j].RelatedTime)
+	})
+	return matchKyousList, nil
 	// ˄
 }
 
-func (r *Repositories) FindKyous(ctx context.Context, queryJSON string) ([]*Kyou, error) {
+func (r Repositories) Close(ctx context.Context) error {
 	// ˅
-	panic("notImplements")
+	existErr := false
+	var err error
+	wg := &sync.WaitGroup{}
+	errch := make(chan error, len(r))
+	defer close(errch)
+
+	// 並列処理
+	for _, rep := range r {
+		wg.Add(1)
+		rep := rep
+		go func(rep Repository) {
+			defer wg.Done()
+			err = rep.Close(ctx)
+			if err != nil {
+				errch <- err
+				return
+			}
+		}(rep)
+	}
+	wg.Wait()
+
+	// エラー集約
+errloop:
+	for {
+		select {
+		case e := <-errch:
+			err = fmt.Errorf("error at close: %w", e)
+			existErr = true
+		default:
+			break errloop
+		}
+	}
+	if existErr {
+		return err
+	}
+
+	return nil
 	// ˄
 }
 
-func (r *Repositories) Close(ctx context.Context) error {
+func (r Repositories) GetKyou(ctx context.Context, id string) (*Kyou, error) {
 	// ˅
-	panic("notImplements")
+	matchKyou := &Kyou{}
+	matchKyou = nil
+	existErr := false
+	var err error
+	wg := &sync.WaitGroup{}
+	ch := make(chan *Kyou, len(r))
+	errch := make(chan error, len(r))
+	defer close(ch)
+	defer close(errch)
+
+	// 並列処理
+	for _, rep := range r {
+		wg.Add(1)
+		rep := rep
+		go func(rep Repository) {
+			defer wg.Done()
+			matchKyouInRep, err := rep.GetKyou(ctx, id)
+			if err != nil {
+				errch <- err
+				return
+			}
+			ch <- matchKyouInRep
+		}(rep)
+	}
+	wg.Wait()
+
+	// エラー集約
+errloop:
+	for {
+		select {
+		case e := <-errch:
+			err = fmt.Errorf("error at get kyou: %w", e)
+			existErr = true
+		default:
+			break errloop
+		}
+	}
+	if existErr {
+		return nil, err
+	}
+
+	// Kyou集約。UpdateTimeが最新のものを収める
+loop:
+	for {
+		select {
+		case matchKyouInRep := <-ch:
+			if matchKyouInRep == nil {
+				continue loop
+			}
+			if matchKyou != nil {
+				if matchKyouInRep.UpdateTime.Before(matchKyou.UpdateTime) {
+					matchKyou = matchKyouInRep
+				}
+			} else {
+				matchKyou = matchKyouInRep
+			}
+		default:
+			break loop
+		}
+	}
+
+	return matchKyou, nil
 	// ˄
 }
 
-func (r *Repositories) GetKyou(ctx context.Context, id string) (*Kyou, error) {
+func (r Repositories) UpdateCache(ctx context.Context) error {
 	// ˅
-	panic("notImplements")
+	existErr := false
+	var err error
+	wg := &sync.WaitGroup{}
+	errch := make(chan error, len(r))
+	defer close(errch)
+
+	// 並列処理
+	for _, rep := range r {
+		wg.Add(1)
+		rep := rep
+		go func(rep Repository) {
+			defer wg.Done()
+			err = rep.UpdateCache(ctx)
+			if err != nil {
+				errch <- err
+				return
+			}
+		}(rep)
+	}
+	wg.Wait()
+
+	// エラー集約
+errloop:
+	for {
+		select {
+		case e := <-errch:
+			err = fmt.Errorf("error at update cache: %w", e)
+			existErr = true
+		default:
+			break errloop
+		}
+	}
+	if existErr {
+		return err
+	}
+
+	return nil
 	// ˄
 }
 
-func (r *Repositories) UpdateCache(ctx context.Context) error {
+func (r Repositories) GetPath(ctx context.Context, id string) (string, error) {
 	// ˅
-	panic("notImplements")
+	err := fmt.Errorf("not implements Reps.GetPath")
+	return "", err
 	// ˄
 }
 
-func (r *Repositories) GetPath(ctx context.Context, id string) (string, error) {
+func (r Repositories) GetRepName(ctx context.Context) (string, error) {
 	// ˅
-	panic("notImplements")
+	return "Reps", nil
 	// ˄
 }
 
-func (r *Repositories) GetRepName(ctx context.Context) (string, error) {
+func (r Repositories) GetKyouHistories(ctx context.Context, id string) ([]*Kyou, error) {
 	// ˅
-	panic("notImplements")
-	// ˄
-}
+	kyouHistories := map[string]*Kyou{}
+	existErr := false
+	var err error
+	wg := &sync.WaitGroup{}
+	ch := make(chan []*Kyou, len(r))
+	errch := make(chan error, len(r))
+	defer close(ch)
+	defer close(errch)
 
-func (r *Repositories) GetKyouHistories(ctx context.Context, id string) ([]*Kyou, error) {
-	// ˅
-	panic("notImplements")
+	// 並列処理
+	for _, rep := range r {
+		wg.Add(1)
+		rep := rep
+		go func(rep Repository) {
+			defer wg.Done()
+			matchKyousInRep, err := rep.GetKyouHistories(ctx, id)
+			if err != nil {
+				errch <- err
+				return
+			}
+			ch <- matchKyousInRep
+		}(rep)
+	}
+	wg.Wait()
+
+	// エラー集約
+errloop:
+	for {
+		select {
+		case e := <-errch:
+			err = fmt.Errorf("error at get kyou histories: %w", e)
+			existErr = true
+		default:
+			break errloop
+		}
+	}
+	if existErr {
+		return nil, err
+	}
+
+	// Kyou集約。UpdateTimeが最新のものを収める
+loop:
+	for {
+		select {
+		case matchKyousInRep := <-ch:
+			if matchKyousInRep == nil {
+				continue loop
+			}
+			for _, kyou := range matchKyousInRep {
+				if existKyou, exist := kyouHistories[kyou.ID+kyou.UpdateTime.Format(sqlite3impl.TimeLayout)]; exist {
+					if kyou.UpdateTime.Before(existKyou.UpdateTime) {
+						kyouHistories[kyou.ID+kyou.UpdateTime.Format(sqlite3impl.TimeLayout)] = kyou
+					}
+				} else {
+					kyouHistories[kyou.ID+kyou.UpdateTime.Format(sqlite3impl.TimeLayout)] = kyou
+				}
+			}
+		default:
+			break loop
+		}
+	}
+
+	kyouHistoriesList := []*Kyou{}
+	for _, kyou := range kyouHistories {
+		if kyou == nil {
+			continue
+		}
+		kyouHistoriesList = append(kyouHistoriesList, kyou)
+	}
+
+	sort.Slice(kyouHistoriesList, func(i, j int) bool {
+		return kyouHistoriesList[i].UpdateTime.After(kyouHistoriesList[j].UpdateTime)
+	})
+
+	return kyouHistoriesList, nil
 	// ˄
 }
 
