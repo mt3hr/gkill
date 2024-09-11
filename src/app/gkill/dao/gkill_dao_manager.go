88d3c2a29889ceedd52dbe7e48bcdb6c095ab77f
mt3hr <ymt3shr@gmail.com>
2024-09-11 17:16:55 +0900

    implements main関数 コミット漏れ

--- src/app/gkill/dao/gkill_dao_manager.go
+++ src/app/gkill/dao/gkill_dao_manager.go
@@ -4,13 +4,19 @@ package dao
 import (
 	"context"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"sort"
 
 	"github.com/gorilla/mux"
 	"github.com/mattn/go-zglob"
+	"github.com/mt3hr/gkill/src/app/gkill/dao/account"
+	"github.com/mt3hr/gkill/src/app/gkill/dao/account_state"
+	"github.com/mt3hr/gkill/src/app/gkill/dao/mi_share_info"
 	"github.com/mt3hr/gkill/src/app/gkill/dao/reps"
+	"github.com/mt3hr/gkill/src/app/gkill/dao/server_config"
+	"github.com/mt3hr/gkill/src/app/gkill/dao/user_config"
 )
 
 // ˄
@@ -33,8 +39,9 @@ type GkillDAOManager struct {
 	// ˄
 }
 
-func NewGkillDAOManager(autoIDF bool) *GkillDAOManager {
-	return &GkillDAOManager{
+func NewGkillDAOManager(autoIDF bool) (*GkillDAOManager, error) {
+	ctx := context.Background()
+	gkillDAOManager := &GkillDAOManager{
 		router:  &mux.Router{},
 		autoIDF: &autoIDF,
 		IDFIgnore: []string{
@@ -49,6 +56,65 @@ func NewGkillDAOManager(autoIDF bool) *GkillDAOManager {
 			".localized",
 		},
 	}
+
+	configDBRootDir := os.ExpandEnv("$HOME/gkill/configs")
+	err := os.MkdirAll(configDBRootDir, fs.ModePerm)
+	if err != nil {
+		err = fmt.Errorf("error at create directory %s: %w", err)
+		return nil, err
+	}
+
+	gkillDAOManager.ConfigDAOs = &ConfigDAOs{}
+	gkillDAOManager.ConfigDAOs.ServerConfigDAO, err = server_config.NewServerConfigDAOSQLite3Impl(ctx, filepath.Join(configDBRootDir, "server_config.db"))
+	if err != nil {
+		return nil, err
+	}
+	gkillDAOManager.ConfigDAOs.AccountDAO, err = account.NewAccountDAOSQLite3Impl(ctx, filepath.Join(configDBRootDir, "account.db"))
+	if err != nil {
+		return nil, err
+	}
+	gkillDAOManager.ConfigDAOs.LoginSessionDAO, err = account_state.NewLoginSessionDAOSQLite3Impl(ctx, filepath.Join(configDBRootDir, "account_state.db"))
+	if err != nil {
+		return nil, err
+	}
+	gkillDAOManager.ConfigDAOs.FileUploadHistoryDAO, err = account_state.NewFileUploadHistoryDAOSQLite3Impl(ctx, filepath.Join(configDBRootDir, "account_state.db"))
+	if err != nil {
+		return nil, err
+	}
+	gkillDAOManager.ConfigDAOs.MiShareInfoDAO, err = mi_share_info.NewMiShareInfoDAOSQLite3Impl(ctx, filepath.Join(configDBRootDir, "mi_share_info.db"))
+	if err != nil {
+		return nil, err
+	}
+	gkillDAOManager.ConfigDAOs.AppllicationConfigDAO, err = user_config.NewApplicationConfigDAOSQLite3Impl(ctx, filepath.Join(configDBRootDir, "user_config.db"))
+	if err != nil {
+		return nil, err
+	}
+	gkillDAOManager.ConfigDAOs.RepositoryDAO, err = user_config.NewRepositoryDAOSQLite3Impl(ctx, filepath.Join(configDBRootDir, "user_config.db"))
+	if err != nil {
+		return nil, err
+	}
+	gkillDAOManager.ConfigDAOs.KFTLTemplateDAO, err = user_config.NewKFTLTemplateDAOSQLite3Impl(ctx, filepath.Join(configDBRootDir, "user_config.db"))
+	if err != nil {
+		return nil, err
+	}
+	gkillDAOManager.ConfigDAOs.TagStructDAO, err = user_config.NewTagStructDAOSQLite3Impl(ctx, filepath.Join(configDBRootDir, "user_config.db"))
+	if err != nil {
+		return nil, err
+	}
+	gkillDAOManager.ConfigDAOs.RepStructDAO, err = user_config.NewRepStructDAOSQLite3Impl(ctx, filepath.Join(configDBRootDir, "user_config.db"))
+	if err != nil {
+		return nil, err
+	}
+	gkillDAOManager.ConfigDAOs.DeviceStructDAO, err = user_config.NewDeviceStructDAOSQLite3Impl(ctx, filepath.Join(configDBRootDir, "user_config.db"))
+	if err != nil {
+		return nil, err
+	}
+	gkillDAOManager.ConfigDAOs.RepTypeStructDAO, err = user_config.NewRepTypeStructDAOSQLite3Impl(ctx, filepath.Join(configDBRootDir, "user_config.db"))
+	if err != nil {
+		return nil, err
+	}
+
+	return gkillDAOManager, nil
 }
 
 func (g *GkillDAOManager) GetRouter() *mux.Router {
---
+++ src/app/gkill/main/common/common.go
@@ -0,0 +1,69 @@
+package common
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"runtime"
+
+	"github.com/mt3hr/gkill/src/app/gkill/api"
+)
+
+var (
+	gkillServerAPI *api.GkillServerAPI
+)
+
+func init() {
+	if "" == os.ExpandEnv("$HOME") {
+		os.Setenv("HOME", os.ExpandEnv("$HOMEPATH"))
+	}
+}
+
+func InitGkillServerAPI() error {
+	var err error
+	gkillServerAPI, err = api.NewGkillServerAPI()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func LaunchGkillServerAPI() error {
+	var err error
+
+	defer gkillServerAPI.Close()
+	interceptCh := make(chan os.Signal)
+	signal.Notify(interceptCh, os.Interrupt)
+	go func() {
+		<-interceptCh
+		gkillServerAPI.Close()
+		os.Exit(0)
+	}()
+
+	err = gkillServerAPI.Serve()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetGkillServerAPI() *api.GkillServerAPI {
+	return gkillServerAPI
+}
+
+func Openbrowser(url string) error {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	return err
+}
