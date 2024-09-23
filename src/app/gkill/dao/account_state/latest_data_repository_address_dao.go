fc0faefa29273b86be54fc73fcbca9925fc680b5
mt3hr <38493486+mt3hr@users.noreply.github.com>
2024-09-23 22:03:43 +0900

    implmeents LatestDataRepositoryAddressDAO

---
+++ src/app/gkill/dao/account_state/latest_data_repository_address_dao.go
@@ -0,0 +1,18 @@
+package account_state
+
+import (
+	"context"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type LatestDataRepositoryAddressDAO interface {
+	GetAllLatestDataRepositoryAddresses(ctx context.Context) ([]*LatestDataRepositoryAddress, error)
+	GetLatestDataRepositoryAddress(ctx context.Context, targetID string) (*LatestDataRepositoryAddress, error)
+	AddLatestDataRepositoryAddress(ctx context.Context, latestDataRepositoryAddress *LatestDataRepositoryAddress) (bool, error)
+	UpdateLatestDataRepositoryAddress(ctx context.Context, latestDataRepositoryAddress *LatestDataRepositoryAddress) (bool, error)
+	UpdateOrAddLatestDataRepositoryAddress(ctx context.Context, latestDataRepositoryAddress *LatestDataRepositoryAddress) (bool, error)
+	UpdateOrAddLatestDataRepositoryAddresses(ctx context.Context, latestDataRepositoryAddresses []*LatestDataRepositoryAddress) (bool, error)
+	DeleteLatestDataRepositoryAddress(ctx context.Context, latestDataRepositoryAddress *LatestDataRepositoryAddress) (bool, error)
+	Close(ctx context.Context) error
+}
