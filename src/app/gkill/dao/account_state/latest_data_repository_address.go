c2b0a20bb4eb85cd0f90cef908c3459c1b3d4461
mt3hr <38493486+mt3hr@users.noreply.github.com>
2024-09-23 22:03:43 +0900

    implmeents LatestDataRepositoryAddressDAO

---
+++ src/app/gkill/dao/account_state/latest_data_repository_address.go
@@ -0,0 +1,13 @@
+package account_state
+
+import (
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type LatestDataRepositoryAddress struct {
+	TargetID                 string    `json:"target_id"`
+	LatestDataRepositoryName string    `json:"latest_data_repository_name"`
+	DataUpdateTime           time.Time `json:"data_update_time"`
+}
