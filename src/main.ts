2b87b33bdfa6bb249e914d105329fe959b2e1ad8
mt3hr <ymt3shr@gmail.com>
2024-09-28 20:23:39 +0900

    implements ログイン画面

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
+import vuetify from "./plugins/vuetify"
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(router);
-app.use(vuetify).mount("#app");
+app.use(router)
+app.use(vuetify)
+
+app.mount("#app")
