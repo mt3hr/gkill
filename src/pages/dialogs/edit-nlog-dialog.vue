a44dbbb01a30b88b9edcb4d32dbc47a6c8843518
mt3hr <38493486+mt3hr@users.noreply.github.com>
2024-10-23 02:16:20 +0900

    implements Nlog画面関連

--- src/pages/dialogs/edit-nlog-dialog.vue
+++ src/pages/dialogs/edit-nlog-dialog.vue
@@ -7,15 +7,6 @@
             @requested_reload_kyou="(kyou) => emits('requested_reload_kyou', kyou)"
             @requested_reload_list="emits('requested_reload_list')"
             @requested_update_check_kyous="(kyou, is_checked) => emits('requested_update_check_kyous', kyou, is_checked)" />
-        <KyouView :application_config="application_config" :gkill_api="gkill_api" :highlight_targets="highlight_targets"
-            :is_image_view="false" :kyou="kyou" :last_added_tag="last_added_tag" :show_checkbox="false"
-            :show_content_only="false" :show_mi_create_time="true" :show_mi_estimate_end_time="true"
-            :show_mi_estimate_start_time="true" :show_mi_limit_time="true" :show_timeis_plaing_end_button="true"
-            @received_errors="(errors) => emits('received_errors', errors)"
-            @received_messages="(messages) => emits('received_messages', messages)"
-            @requested_reload_kyou="(kyou) => emits('requested_reload_kyou', kyou)"
-            @requested_reload_list="emits('requested_reload_list')"
-            @requested_update_check_kyous="(kyou, is_checked) => emits('requested_update_check_kyous', kyou, is_checked)" />
     </v-dialog>
 </template>
 <script setup lang="ts">
@@ -23,7 +14,6 @@ import { type Ref, ref, watch } from 'vue'
 import type { EditNlogDialogProps } from './edit-nlog-dialog-props'
 import type { KyouDialogEmits } from '../views/kyou-dialog-emits'
 import EditNlogView from '../views/edit-nlog-view.vue'
-import KyouView from '../views/kyou-view.vue'
 
 const props = defineProps<EditNlogDialogProps>()
 const emits = defineEmits<KyouDialogEmits>()
