a460b38736e5d479bdcf582d3e036a66a5284d81
mt3hr <38493486+mt3hr@users.noreply.github.com>
2024-10-23 03:18:25 +0900

    implements Lantana画面関連

--- src/pages/views/lantana-view.vue
+++ src/pages/views/lantana-view.vue
@@ -1,9 +1,14 @@
 <template>
+    <v-card @contextmenu.prevent="show_context_menu">
+        <LantanaFlowersView v-if="kyou.typed_lantana" :application_config="application_config" :gkill_api="gkill_api"
+            :editable="false" :mood="kyou.typed_lantana.mood" />
+    </v-card>
     <LantanaContextMenu :application_config="application_config" :gkill_api="gkill_api"
-        :highlight_targets="highlight_targets" :kyou="kyou" :last_added_tag="last_added_tag"
-        @received_errors="(errors) => emits('received_errors', errors)"
+        :highlight_targets="[kyou.generate_info_identifer()]" :kyou="kyou" :last_added_tag="last_added_tag"
+        ref="context_menu" @received_errors="(errors) => emits('received_errors', errors)"
         @received_messages="(messages) => emits('received_messages', messages)"
-        @requested_reload_kyou="(kyou) => emits('requested_reload_kyou', kyou)" @requested_reload_list="() => { }"
+        @requested_reload_kyou="(kyou) => emits('requested_reload_kyou', kyou)"
+        @requested_reload_list="() => emits('requested_reload_list')"
         @requested_update_check_kyous="(kyous, is_checked) => emits('requested_update_check_kyous', kyous, is_checked)" />
 </template>
 <script setup lang="ts">
@@ -13,7 +18,14 @@ import type { Lantana } from '@/classes/datas/lantana'
 import { type Ref, ref } from 'vue'
 import LantanaContextMenu from './lantana-context-menu.vue'
 import type { Kyou } from '@/classes/datas/kyou'
+import LantanaFlowersView from './lantana-flowers-view.vue'
+
+const context_menu = ref<InstanceType<typeof LantanaContextMenu> | null>(null);
 
 const props = defineProps<LantanaViewProps>()
 const emits = defineEmits<KyouViewEmits>()
+
+function show_context_menu(e: PointerEvent): void {
+    context_menu.value?.show(e)
+}
 </script>
