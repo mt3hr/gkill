abd48db5558a241aa9b61751bbad9cad1c82ce68
mt3hr <38493486+mt3hr@users.noreply.github.com>
2024-10-23 03:18:25 +0900

    implements Lantana画面関連

--- src/pages/views/lantana-flower.vue
+++ src/pages/views/lantana-flower.vue
@@ -1,15 +1,103 @@
 <template>
-    <img class="lantana_icon_harf_right" />
-    <img class="lantana_icon_harf_left" />
-    <img class="lantana_icon_full_left" />
-    <img class="lantana_icon_harf_left_gray" />
-    <img class="lantana_icon_harf_right_gray" />
-    <img class="lantana_icon_full_left_gray" />
+    <div class="lantana_icon">
+        <img v-if="state == LantanaFlowerState.full" class="lantana_icon_harf_left" :src="lantana_icon"
+            @click="emit_clicked_left" />
+        <img v-if="state == LantanaFlowerState.full" class="lantana_icon_harf_right" :src="lantana_icon"
+            @click="emit_clicked_right" />
+        <img v-if="state == LantanaFlowerState.half" class="lantana_icon_harf_left" :src="lantana_icon"
+            @click="emit_clicked_left" />
+        <img v-if="state == LantanaFlowerState.half" class="lantana_icon_harf_right gray" :src="lantana_icon"
+            @click="emit_clicked_right" />
+        <img v-if="state == LantanaFlowerState.none" class="lantana_icon_harf_left gray" :src="lantana_icon"
+            @click="emit_clicked_left" />
+        <img v-if="state == LantanaFlowerState.none" class="lantana_icon_harf_right gray" :src="lantana_icon"
+            @click="emit_clicked_right" />
+    </div>
 </template>
 <script lang="ts" setup>
 import type { LantanaFlowerEmits } from './lantana-flower-emits'
 import type { LantanaFlowerProps } from './lantana-flower-props'
+import { LantanaFlowerState } from '@/classes/lantana/lantana-flower-state';
+import lantana_icon from '/public/lantana_icon.png'
 
 const props = defineProps<LantanaFlowerProps>()
 const emits = defineEmits<LantanaFlowerEmits>()
+
+function emit_clicked_left() {
+    if (props.editable) {
+        emits('clicked_left')
+    }
+}
+function emit_clicked_right() {
+    if (props.editable) {
+        emits('clicked_right')
+    }
+}
 </script>
+
+<style scoped>
+.lantana_icon {
+    position: relative;
+    width: 50px !important;
+    height: 50px !important;
+    max-width: 50px !important;
+    min-width: 50px !important;
+    max-height: 50px !important;
+    min-height: 50px !important;
+    display: inline-block;
+}
+
+.lantana_icon_fill {
+    width: 50px !important;
+    height: 50px !important;
+    max-width: 50px !important;
+    min-width: 50px !important;
+    max-height: 50px !important;
+    min-height: 50px !important;
+    display: inline-block;
+    z-index: 10;
+}
+
+.lantana_icon_harf_left {
+    position: absolute;
+    left: 0px;
+    width: 25px !important;
+    height: 50px !important;
+    max-width: 25px !important;
+    min-width: 25px !important;
+    max-height: 50px !important;
+    min-height: 50px !important;
+    object-fit: cover;
+    object-position: 0 0;
+    display: inline-block;
+    z-index: 10;
+}
+
+.lantana_icon_harf_right {
+    position: absolute;
+    left: 0px;
+    width: 50px !important;
+    height: 50px !important;
+    max-width: 50px !important;
+    min-width: 50px !important;
+    max-height: 50px !important;
+    min-height: 50px !important;
+    display: inline-block;
+    z-index: 9;
+}
+
+.lantana_icon_none {
+    width: 50px !important;
+    height: 50px !important;
+    max-width: 50px !important;
+    min-width: 50px !important;
+    max-height: 50px !important;
+    min-height: 50px !important;
+    display: inline-block;
+    z-index: 10;
+}
+
+.gray {
+    filter: grayscale(100%);
+}
+</style>
--- src/pages/views/lantana-flowers-view.vue
+++ src/pages/views/lantana-flowers-view.vue
@@ -1,18 +1,32 @@
 <template>
-    <LantanaFlower :application_config="application_config" :editable="editable" :gkill_api="gkill_api"
-        :state="flower_state_1" @clicked_left="set_mood(1)" @clicked_right="set_mood(2)" />
-    <LantanaFlower :application_config="application_config" :editable="editable" :gkill_api="gkill_api"
-        :state="flower_state_2" @clicked_left="set_mood(3)" @clicked_right="set_mood(4)" />
-    <LantanaFlower :application_config="application_config" :editable="editable" :gkill_api="gkill_api"
-        :state="flower_state_3" @clicked_left="set_mood(5)" @clicked_right="set_mood(6)" />
-    <LantanaFlower :application_config="application_config" :editable="editable" :gkill_api="gkill_api"
-        :state="flower_state_4" @clicked_left="set_mood(7)" @clicked_right="set_mood(8)" />
-    <LantanaFlower :application_config="application_config" :editable="editable" :gkill_api="gkill_api"
-        :state="flower_state_5" @clicked_left="set_mood(9)" @clicked_right="set_mood(10)" />
+    <table>
+        <tr class="lantana_icon_tr">
+            <td class="lantana_icon_td">
+                <LantanaFlower :application_config="application_config" :editable="editable" :gkill_api="gkill_api"
+                    :state="flower_state_1" @clicked_left="set_mood(1)" @clicked_right="set_mood(2)" />
+            </td>
+            <td class="lantana_icon_td">
+                <LantanaFlower :application_config="application_config" :editable="editable" :gkill_api="gkill_api"
+                    :state="flower_state_2" @clicked_left="set_mood(3)" @clicked_right="set_mood(4)" />
+            </td>
+            <td class="lantana_icon_td">
+                <LantanaFlower :application_config="application_config" :editable="editable" :gkill_api="gkill_api"
+                    :state="flower_state_3" @clicked_left="set_mood(5)" @clicked_right="set_mood(6)" />
+            </td>
+            <td class="lantana_icon_td">
+                <LantanaFlower :application_config="application_config" :editable="editable" :gkill_api="gkill_api"
+                    :state="flower_state_4" @clicked_left="set_mood(7)" @clicked_right="set_mood(8)" />
+            </td>
+            <td class="lantana_icon_td">
+                <LantanaFlower :application_config="application_config" :editable="editable" :gkill_api="gkill_api"
+                    :state="flower_state_5" @clicked_left="set_mood(9)" @clicked_right="set_mood(10)" />
+            </td>
+        </tr>
+    </table>
     <LantanaTextView />
 </template>
 <script setup lang="ts">
-import { type Ref, ref } from 'vue'
+import { type Ref, ref, watch } from 'vue'
 import type { LantanaFlowersViewEmits } from './lantana-flowers-view-emits'
 import type { LantanaFlowersViewProps } from './lantana-flowers-view-props'
 import LantanaFlower from './lantana-flower.vue'
@@ -23,19 +37,55 @@ const props = defineProps<LantanaFlowersViewProps>()
 const emits = defineEmits<LantanaFlowersViewEmits>()
 defineExpose({ set_mood, get_mood })
 
-const flower_state_1: Ref<LantanaFlowerState> = ref(LantanaFlowerState.none)
-const flower_state_2: Ref<LantanaFlowerState> = ref(LantanaFlowerState.none)
-const flower_state_3: Ref<LantanaFlowerState> = ref(LantanaFlowerState.none)
-const flower_state_4: Ref<LantanaFlowerState> = ref(LantanaFlowerState.none)
-const flower_state_5: Ref<LantanaFlowerState> = ref(LantanaFlowerState.none)
+const mood: Ref<Number> = ref(props.mood)
+
+const flower_state_1: Ref<LantanaFlowerState> = ref(mood.value.valueOf() >= 2 ? LantanaFlowerState.full : (mood.value.valueOf() >= 1 ? LantanaFlowerState.half : LantanaFlowerState.none))
+const flower_state_2: Ref<LantanaFlowerState> = ref(mood.value.valueOf() >= 4 ? LantanaFlowerState.full : (mood.value.valueOf() >= 3 ? LantanaFlowerState.half : LantanaFlowerState.none))
+const flower_state_3: Ref<LantanaFlowerState> = ref(mood.value.valueOf() >= 6 ? LantanaFlowerState.full : (mood.value.valueOf() >= 5 ? LantanaFlowerState.half : LantanaFlowerState.none))
+const flower_state_4: Ref<LantanaFlowerState> = ref(mood.value.valueOf() >= 8 ? LantanaFlowerState.full : (mood.value.valueOf() >= 7 ? LantanaFlowerState.half : LantanaFlowerState.none))
+const flower_state_5: Ref<LantanaFlowerState> = ref(mood.value.valueOf() >= 10 ? LantanaFlowerState.full : (mood.value.valueOf() >= 9 ? LantanaFlowerState.half : LantanaFlowerState.none))
+
+watch(props.mood, () => {
+    mood.value = props.mood
+})
+
+watch(mood, () => {
+    flower_state_1.value = (mood.value.valueOf() >= 2 ? LantanaFlowerState.full : (mood.value.valueOf() >= 1 ? LantanaFlowerState.half : LantanaFlowerState.none))
+    flower_state_2.value = (mood.value.valueOf() >= 4 ? LantanaFlowerState.full : (mood.value.valueOf() >= 3 ? LantanaFlowerState.half : LantanaFlowerState.none))
+    flower_state_3.value = (mood.value.valueOf() >= 6 ? LantanaFlowerState.full : (mood.value.valueOf() >= 5 ? LantanaFlowerState.half : LantanaFlowerState.none))
+    flower_state_4.value = (mood.value.valueOf() >= 8 ? LantanaFlowerState.full : (mood.value.valueOf() >= 7 ? LantanaFlowerState.half : LantanaFlowerState.none))
+    flower_state_5.value = (mood.value.valueOf() >= 10 ? LantanaFlowerState.full : (mood.value.valueOf() >= 9 ? LantanaFlowerState.half : LantanaFlowerState.none))
+    emit_updated_mood()
+})
 
 async function get_mood(): Promise<Number> {
-    throw new Error('Not implemented')
+    return mood.value
 }
-async function set_mood(mood: Number): Promise<void> {
-    throw new Error('Not implemented')
+async function set_mood(mood_value: Number): Promise<void> {
+    if (!props.editable) {
+        return
+    }
+    mood.value = mood_value
 }
 async function emit_updated_mood(): Promise<void> {
-    throw new Error('Not implemented')
+    emits('updated_mood', mood.value)
 }
 </script>
+
+<style scoped>
+.lantana_icon_tr {
+    width: calc(50px * 5);
+    max-width: calc(50px * 5);
+    min-width: calc(50px * 5);
+}
+
+.lantana_icon_td {
+    width: 50px !important;
+    height: 50px !important;
+    max-width: 50px !important;
+    min-width: 50px !important;
+    max-height: 50px !important;
+    min-height: 50px !important;
+    display: inline-block;
+}
+</style>
