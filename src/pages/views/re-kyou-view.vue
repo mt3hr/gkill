07083728ad1a92fc70dae2289706c9fa9a2d6e91
mt3hr <38493486+mt3hr@users.noreply.github.com>
2024-10-23 12:23:29 +0900

    implements KyouListView

--- src/pages/views/re-kyou-view.vue
+++ src/pages/views/re-kyou-view.vue
@@ -8,7 +8,8 @@
             @received_errors="(errors) => emits('received_errors', errors)"
             @received_messages="(messages) => emits('received_messages', messages)"
             @requested_reload_kyou="(kyou) => emits('requested_reload_kyou', kyou)" @requested_reload_list="() => { }"
-            @requested_update_check_kyous="(kyous, is_checked) => emits('requested_update_check_kyous', kyous, is_checked)" />
+            @requested_update_check_kyous="(kyous, is_checked) => emits('requested_update_check_kyous', kyous, is_checked)"
+            @dblclick.prevent.stop="() => {}" />
     </v-card>
     <ReKyouContextMenu :application_config="application_config" :gkill_api="gkill_api"
         :highlight_targets="[kyou.generate_info_identifer()]" :kyou="kyou" :last_added_tag="last_added_tag"
