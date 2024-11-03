d8e113caefb112557065439916f7ade9ec88fee7
mt3hr <ymt3shr@gmail.com>
2024-11-03 17:05:58 +0900

    implements rykv画面サイドバーCalendarコンポーネント

--- src/pages/views/calendar-query.vue
+++ src/pages/views/calendar-query.vue
@@ -1,11 +1,85 @@
 <template>
-
+    <v-row class="pa-0 ma-0">
+        <v-col cols="auto" class="pa-0 ma-0">
+            <v-checkbox v-model="use_calendar" @click="clicked_use_calendar_checkbox" label="日付" hide-details />
+        </v-col>
+        <v-spacer />
+        <v-col cols="auto" class="pa-0 ma-0">
+            <v-btn @click="clicked_clear_calendar_button">クリア</v-btn>
+        </v-col>
+    </v-row>
+    <VDatePicker :max-width="300" :model-value="dates" :color="'primary'" :multiple="'range'"
+        @wheel="(e: any) => on_wheel(e)" @update:model-value="clicked_date" />
+    <v-slider min="0" max="86399" v-model="slider_model" :label="time" />
 </template>
 <script lang="ts" setup>
+import moment from 'moment';
 import type { CalendarQueryEmits } from './calendar-query-emits'
 import type { CalendarQueryProps } from './calendar-query-props'
-import { ref } from 'vue'
+import { computed, ref, type Ref, defineEmits, defineProps, watch } from 'vue'
+import { VDatePicker } from 'vuetify/components';
 
 const props = defineProps<CalendarQueryProps>()
 const emits = defineEmits<CalendarQueryEmits>()
+
+const date_picker = ref<InstanceType<typeof VDatePicker> | null>(null)
+
+const slider_model: Ref<Number> = ref(86399)
+const dates: Ref<Array<Date>> = ref([moment('2024-11-01').toDate(), moment('2024-11-03').toDate()])
+const use_calendar: Ref<boolean> = ref(false)
+
+watch(props.query, () => {
+    use_calendar.value = props.query.use_calendar
+
+    dates.value = []
+    const start_date = moment(props.query.calendar_start_date)
+    const end_date = moment(props.query.calendar_end_date)
+    const date_list = Array<Date>()
+    for (let date = start_date; date.unix() !== end_date.unix(); date = date.add('days', 1)) {
+        date_list.push(date.toDate())
+    }
+    dates.value = date_list
+})
+
+
+const time = computed(() => {
+    return ('00' + (slider_model.value.valueOf() / 3600)).slice(-2) + ":" +
+        ('00' + ((slider_model.value.valueOf() / 60) % 60)).slice(-2) + ":" +
+        ('00' + (slider_model.value.valueOf() % 60)).slice(-2)
+})
+
+// 日付がクリックされた時、日時を更新してclicked_timeをemitする
+function clicked_date(recved_dates: any): void {
+    dates.value = recved_dates
+    console.log(dates.value)
+    if (dates.value && dates.value.length !== 0) {
+        emits('request_update_dates', moment(dates.value[0]).toDate(), moment(dates.value[dates.value.length - 1]).toDate())
+    } else {
+        emits('request_update_dates', null, null)
+    }
+}
+// カレンダーでホイールが転がされた時、下ならカレンダーを次の年月へ、上ならカレンダーを前の年月へ
+function on_wheel(e: any) {
+    if (0 < e.deltaY) {
+        document.querySelectorAll("div.v-sheet.v-picker.v-date-picker.v-date-picker--month > div.v-picker__body > div.v-date-picker-controls > div.v-date-picker-controls__month > button:nth-child(1)").forEach((el, key, parent) => { (el as any).click() })
+    } else {
+        document.querySelectorAll("div.v-sheet.v-picker.v-date-picker.v-date-picker--month > div.v-picker__body > div.v-date-picker-controls > div.v-date-picker-controls__month > button:nth-child(2)").forEach((el, key, parent) => { (el as any).click() })
+    }
+}
+
+function clicked_clear_calendar_button(): void {
+    dates.value = []
+    //emits('request_update_dates', null, null)
+}
+
+function clicked_use_calendar_checkbox(): void {
+    emits('request_update_use_calendar_query', use_calendar.value)
+}
 </script>
+<style lang="css">
+div.v-sheet.v-picker.v-date-picker.v-date-picker--year>div:nth-child(1),
+div.v-sheet.v-picker.v-date-picker.v-date-picker--month>div:nth-child(1),
+div.v-sheet.v-picker.v-date-picker.v-date-picker--months>div:nth-child(1) {
+    display: none;
+}
+</style>
--- src/pages/views/rykv-query-editor-side-bar.vue
+++ src/pages/views/rykv-query-editor-side-bar.vue
@@ -1,32 +1,35 @@
 <template>
-    <SidebarHeader :application_config="application_config" :gkill_api="gkill_api" :query="query"
-        @request_search="emits('request_search')" @request_clear_find_query="emits('updated_query', default_query)" />
-    <KeywordQuery :application_config="application_config" :gkill_api="gkill_api" :query="query"
-        @request_update_and_search="emits('updated_query', generate_query())"
-        @request_update_keywords="emits('updated_query', generate_query())"
-        @request_update_use_keyword_query="emits('updated_query', generate_query())"
-        @request_clear_keyword_query="emits('updated_query', generate_cleard_keyword_query())" />
-    <TimeIsQuery :application_config="application_config" :gkill_api="gkill_api" :query="query"
-        @request_update_and_search_timeis_tags="emits('updated_query', generate_query())"
-        @request_update_and_search_timeis_word="emits('updated_query', generate_query())"
-        @request_update_timeis_keywords="emits('updated_query', generate_query())"
-        @request_update_use_timeis_query="emits('updated_query', generate_query())"
-        @request_clear_timeis_query="emits('updated_query', generate_cleard_timeis_query())" />
-    <RepQuery :application_config="application_config" :gkill_api="gkill_api" :query="query"
-        @request_update_checked_reps="emits('updated_query', generate_query())"
-        @request_clear_rep_query="emits('updated_query', generate_cleard_rep_query())" />
-    <TagQuery :application_config="application_config" :gkill_api="gkill_api" :query="query"
-        @request_update_and_search_tags="emits('updated_query', generate_query())"
-        @request_update_checked_tags="emits('updated_query', generate_query())"
-        @request_clear_tag_query="emits('updated_query', generate_cleard_tag_query())" />
-    <MapQuery :application_config="application_config" :gkill_api="gkill_api" :query="query"
-        @request_update_area="emits('updated_query', generate_query())"
-        @request_update_use_map_query="emits('updated_query', generate_query())"
-        @request_clear_map_query="emits('updated_query', generate_cleard_map_query())" />
-    <CalendarQuery :application_config="application_config" :gkill_api="gkill_api" :query="query"
-        @request_update_dates="emits('updated_query', generate_query())"
-        @request_update_use_calendar_query="emits('updated_query', generate_query())"
-        @request_clear_calendar_query="emits('updated_query', generate_cleard_calendar_query())" />
+    <div class="rykv_sidebar">
+        <SidebarHeader :application_config="application_config" :gkill_api="gkill_api" :query="query"
+            @request_search="emits('request_search')"
+            @request_clear_find_query="emits('updated_query', default_query)" />
+        <KeywordQuery :application_config="application_config" :gkill_api="gkill_api" :query="query"
+            @request_update_and_search="emits('updated_query', generate_query())"
+            @request_update_keywords="emits('updated_query', generate_query())"
+            @request_update_use_keyword_query="emits('updated_query', generate_query())"
+            @request_clear_keyword_query="emits('updated_query', generate_cleard_keyword_query())" />
+        <TimeIsQuery :application_config="application_config" :gkill_api="gkill_api" :query="query"
+            @request_update_and_search_timeis_tags="emits('updated_query', generate_query())"
+            @request_update_and_search_timeis_word="emits('updated_query', generate_query())"
+            @request_update_timeis_keywords="emits('updated_query', generate_query())"
+            @request_update_use_timeis_query="emits('updated_query', generate_query())"
+            @request_clear_timeis_query="emits('updated_query', generate_cleard_timeis_query())" />
+        <RepQuery :application_config="application_config" :gkill_api="gkill_api" :query="query"
+            @request_update_checked_reps="emits('updated_query', generate_query())"
+            @request_clear_rep_query="emits('updated_query', generate_cleard_rep_query())" />
+        <TagQuery :application_config="application_config" :gkill_api="gkill_api" :query="query"
+            @request_update_and_search_tags="emits('updated_query', generate_query())"
+            @request_update_checked_tags="emits('updated_query', generate_query())"
+            @request_clear_tag_query="emits('updated_query', generate_cleard_tag_query())" />
+        <MapQuery :application_config="application_config" :gkill_api="gkill_api" :query="query"
+            @request_update_area="emits('updated_query', generate_query())"
+            @request_update_use_map_query="emits('updated_query', generate_query())"
+            @request_clear_map_query="emits('updated_query', generate_cleard_map_query())" />
+        <CalendarQuery :application_config="application_config" :gkill_api="gkill_api" :query="query"
+            @request_update_dates="emits('updated_query', generate_query())"
+            @request_update_use_calendar_query="emits('updated_query', generate_query())"
+            @request_clear_calendar_query="emits('updated_query', generate_cleard_calendar_query())" />
+    </div>
 </template>
 <script setup lang="ts">
 import { FindKyouQuery } from '@/classes/api/find_query/find-kyou-query'
