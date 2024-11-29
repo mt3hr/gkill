cc2e2f7ff8bd98b7c58ecfe447a7274dc36b2886
mt3hr <ymt3shr@gmail.com>
2024-11-29 18:25:54 +0900

    動作確認

--- src/classes/kftl/kftl_timeis/kftl-time-is-end-time-statement-line.ts
+++ src/classes/kftl/kftl_timeis/kftl-time-is-end-time-statement-line.ts
@@ -7,6 +7,7 @@ import type { KFTLStatementLineContext } from '../kftl-statement-line-context'
 import { KFTLPrototypeRequest } from '../kftl_prototype/kftl-prototype-request'
 import type { KFTLTimeIsRequest } from './kftl-time-is-request'
 import { KFTLStatementLineConstructorFactory } from '../kftl-statement-line-constructor-factory'
+import { GkillAPI } from '@/classes/api/gkill-api'
 
 export class KFTLTimeIsEndTimeStatementLine extends KFTLStatementLine {
 
@@ -17,17 +18,16 @@ export class KFTLTimeIsEndTimeStatementLine extends KFTLStatementLine {
     }
 
     async apply_this_line_to_request_map(request_map: KFTLRequestMap): Promise<void> {
-        let request = request_map.get(this.get_context().get_this_statement_line_target_id()) as unknown as KFTLTimeIsRequest
+        let request = request_map.get(this.get_context().get_this_statement_line_target_id()) as KFTLTimeIsRequest
         if (!request) {
             request_map.set(this.get_context().get_this_statement_line_target_id(), new KFTLPrototypeRequest(this.get_context().get_this_statement_line_target_id(), this.get_context()))
-            request = request_map.get(this.get_context().get_this_statement_line_target_id()) as unknown as KFTLTimeIsRequest
+            request = request_map.get(this.get_context().get_this_statement_line_target_id()) as KFTLTimeIsRequest
         }
         const time = moment(this.get_context().get_this_statement_line_text().replace("？", "")).toDate()
         if (Number.isNaN(time.getTime())) {
             throw new Error("日時の解釈に失敗しました")
         }
         request.set_end_time(time)
-        return new Promise<void>((resolve) => resolve())
     }
 
     get_label_name(context: KFTLStatementLineContext): string {
