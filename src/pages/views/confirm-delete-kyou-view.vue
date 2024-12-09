cfd3c3b3ad3e97b92ad0f7d729aa9aa7b9ad632e
mt3hr <ymt3shr@gmail.com>
2024-12-09 16:39:31 +0900

    Implements Kyou削除処理

--- src/pages/views/confirm-delete-kyou-view.vue
+++ src/pages/views/confirm-delete-kyou-view.vue
@@ -22,8 +22,7 @@
                 :highlight_targets="highlight_targets" :is_image_view="false" :kyou="kyou"
                 :last_added_tag="last_added_tag" :show_checkbox="false" :show_content_only="false"
                 :show_mi_create_time="true" :show_mi_estimate_end_time="true" :show_mi_estimate_start_time="true"
-                :show_mi_limit_time="true" :show_timeis_plaing_end_button="true"
-                :height="'100%'" :width="'100%'"
+                :show_mi_limit_time="true" :show_timeis_plaing_end_button="true" :height="'100%'" :width="'100%'"
                 @received_errors="(errors) => emits('received_errors', errors)"
                 @received_messages="(messages) => emits('received_messages', messages)"
                 @requested_reload_kyou="(kyou) => emits('requested_reload_kyou', kyou)"
@@ -36,48 +35,211 @@
 import { GetGkillInfoRequest } from '@/classes/api/req_res/get-gkill-info-request';
 import type { ConfirmDeleteKyouViewProps } from './confirm-delete-kyou-view-props'
 import type { KyouViewEmits } from './kyou-view-emits'
-import { type Ref, ref } from 'vue'
+import { type Ref, ref, watch } from 'vue'
 import KyouView from './kyou-view.vue'
 import router from '@/router';
 import { UpdateKyouInfoRequest } from '@/classes/api/req_res/update-kyou-info-request';
 import { GkillAPI } from '@/classes/api/gkill-api';
+import type { Kyou } from '@/classes/datas/kyou';
+import type { GkillError } from '@/classes/api/gkill-error';
+import { UpdateKmemoRequest } from '@/classes/api/req_res/update-kmemo-request';
+import { UpdateURLogRequest } from '@/classes/api/req_res/update-ur-log-request';
+import { UpdateNlogRequest } from '@/classes/api/req_res/update-nlog-request';
+import { UpdateTimeisRequest } from '@/classes/api/req_res/update-timeis-request';
+import { UpdateMiRequest } from '@/classes/api/req_res/update-mi-request';
+import { UpdateLantanaRequest } from '@/classes/api/req_res/update-lantana-request';
+import { UpdateReKyouRequest } from '@/classes/api/req_res/update-re-kyou-request';
 
 const props = defineProps<ConfirmDeleteKyouViewProps>()
 const emits = defineEmits<KyouViewEmits>()
 
+const cloned_kyou: Ref<Kyou> = ref(props.kyou.clone())
+watch(() => props.kyou, () => cloned_kyou.value = props.kyou.clone())
+
 const show_kyou: Ref<boolean> = ref(true)
 
 async function delete_kyou(): Promise<void> {
-    // UserIDやDevice情報を取得する
-    const get_gkill_req = new GetGkillInfoRequest()
-    get_gkill_req.session_id = GkillAPI.get_instance().get_session_id()
-    const gkill_info_res = await props.gkill_api.get_gkill_info(get_gkill_req)
-    if (gkill_info_res.errors && gkill_info_res.errors.length !== 0) {
-        emits('received_errors', gkill_info_res.errors)
-        return
+    let errors = new Array<GkillError>()
+    await cloned_kyou.value.load_typed_datas()
+    if (cloned_kyou.value.data_type.startsWith("kmemo")) {
+        const e = await delete_kmemo()
+        errors = errors.concat(e)
     }
+    if (cloned_kyou.value.data_type.startsWith("urlog")) {
+        const e = await delete_urlog()
+        errors = errors.concat(e)
+    }
+    if (cloned_kyou.value.data_type.startsWith("nlog")) {
+        const e = await delete_nlog()
+        errors = errors.concat(e)
+    }
+    if (cloned_kyou.value.data_type.startsWith("timeis")) {
+        const e = await delete_timeis()
+        errors = errors.concat(e)
+    }
+    if (cloned_kyou.value.data_type.startsWith("mi")) {
+        const e = await delete_mi()
+        errors = errors.concat(e)
+    }
+    if (cloned_kyou.value.data_type.startsWith("lantana")) {
+        const e = await delete_lantana()
+        errors = errors.concat(e)
+    }
+    if (cloned_kyou.value.data_type.startsWith("idf")) {
+        const e = await delete_idf_kyou()
+        errors = errors.concat(e)
+    }
+    if (cloned_kyou.value.data_type.startsWith("git")) {
+        const e = await delete_git_commit_log()
+        errors = errors.concat(e)
+    }
+    if (cloned_kyou.value.data_type.startsWith("rekyou")) {
+        const e = await delete_rekyou()
+        errors = errors.concat(e)
+    }
+    if (errors && errors.length != 0) {
+        emits('received_errors', errors)
+    }
+}
 
-    // 更新後Kyou情報を用意する
-    const updated_kyou = await props.kyou.clone()
-    updated_kyou.is_deleted = true
-    updated_kyou.update_app = "gkill"
-    updated_kyou.update_device = gkill_info_res.device
-    updated_kyou.update_time = new Date(Date.now())
-    updated_kyou.update_user = gkill_info_res.user_id
+async function delete_kmemo(): Promise<Array<GkillError>> {
+    const gkill_info_req = new GetGkillInfoRequest()
+    gkill_info_req.session_id = GkillAPI.get_instance().get_session_id()
+    const gkill_info_res = await GkillAPI.get_instance().get_gkill_info(gkill_info_req)
 
-    // 更新リクエストを飛ばす
-    const req = new UpdateKyouInfoRequest()
+    const req = new UpdateKmemoRequest()
     req.session_id = GkillAPI.get_instance().get_session_id()
-    req.kyou = updated_kyou
-    const res = await props.gkill_api.update_kyou_info(req)
-    if (res.errors && res.errors.length !== 0) {
-        emits('received_errors', res.errors)
-        return
-    }
-    if (res.messages && res.messages.length !== 0) {
-        emits('received_messages', res.messages)
-    }
-    emits('deleted_kyou', res.updated_kyou)
-    return
+    req.kmemo = cloned_kyou.value.typed_kmemo!!.clone()
+    req.kmemo.is_deleted = true
+
+    req.kmemo.update_app = "gkill"
+    req.kmemo.update_device = gkill_info_res.device
+    req.kmemo.update_time = new Date(Date.now())
+    req.kmemo.update_user = gkill_info_res.user_id
+
+    const res = await GkillAPI.get_instance().update_kmemo(req)
+    return res.errors
+}
+async function delete_urlog(): Promise<Array<GkillError>> {
+    const gkill_info_req = new GetGkillInfoRequest()
+    gkill_info_req.session_id = GkillAPI.get_instance().get_session_id()
+    const gkill_info_res = await GkillAPI.get_instance().get_gkill_info(gkill_info_req)
+
+    const req = new UpdateURLogRequest()
+    req.session_id = GkillAPI.get_instance().get_session_id()
+    req.urlog = cloned_kyou.value.typed_urlog!!.clone()
+    req.urlog.is_deleted = true
+
+    req.urlog.update_app = "gkill"
+    req.urlog.update_device = gkill_info_res.device
+    req.urlog.update_time = new Date(Date.now())
+    req.urlog.update_user = gkill_info_res.user_id
+
+    const res = await GkillAPI.get_instance().update_urlog(req)
+    return res.errors
+}
+
+async function delete_nlog(): Promise<Array<GkillError>> {
+    const gkill_info_req = new GetGkillInfoRequest()
+    gkill_info_req.session_id = GkillAPI.get_instance().get_session_id()
+    const gkill_info_res = await GkillAPI.get_instance().get_gkill_info(gkill_info_req)
+
+    const req = new UpdateNlogRequest()
+    req.session_id = GkillAPI.get_instance().get_session_id()
+    req.nlog = cloned_kyou.value.typed_nlog!!.clone()
+    req.nlog.is_deleted = true
+
+    req.nlog.update_app = "gkill"
+    req.nlog.update_device = gkill_info_res.device
+    req.nlog.update_time = new Date(Date.now())
+    req.nlog.update_user = gkill_info_res.user_id
+
+    const res = await GkillAPI.get_instance().update_nlog(req)
+    return res.errors
+}
+
+async function delete_timeis(): Promise<Array<GkillError>> {
+    const gkill_info_req = new GetGkillInfoRequest()
+    gkill_info_req.session_id = GkillAPI.get_instance().get_session_id()
+    const gkill_info_res = await GkillAPI.get_instance().get_gkill_info(gkill_info_req)
+
+    const req = new UpdateTimeisRequest()
+    req.session_id = GkillAPI.get_instance().get_session_id()
+    req.timeis = cloned_kyou.value.typed_timeis!!.clone()
+    req.timeis.is_deleted = true
+
+    req.timeis.update_app = "gkill"
+    req.timeis.update_device = gkill_info_res.device
+    req.timeis.update_time = new Date(Date.now())
+    req.timeis.update_user = gkill_info_res.user_id
+
+    const res = await GkillAPI.get_instance().update_timeis(req)
+    return res.errors
+}
+
+async function delete_mi(): Promise<Array<GkillError>> {
+    const gkill_info_req = new GetGkillInfoRequest()
+    gkill_info_req.session_id = GkillAPI.get_instance().get_session_id()
+    const gkill_info_res = await GkillAPI.get_instance().get_gkill_info(gkill_info_req)
+
+    const req = new UpdateMiRequest()
+    req.session_id = GkillAPI.get_instance().get_session_id()
+    req.mi = cloned_kyou.value.typed_mi!!.clone()
+    req.mi.is_deleted = true
+
+    req.mi.update_app = "gkill"
+    req.mi.update_device = gkill_info_res.device
+    req.mi.update_time = new Date(Date.now())
+    req.mi.update_user = gkill_info_res.user_id
+
+    const res = await GkillAPI.get_instance().update_mi(req)
+    return res.errors
+}
+
+async function delete_lantana(): Promise<Array<GkillError>> {
+    const gkill_info_req = new GetGkillInfoRequest()
+    gkill_info_req.session_id = GkillAPI.get_instance().get_session_id()
+    const gkill_info_res = await GkillAPI.get_instance().get_gkill_info(gkill_info_req)
+
+    const req = new UpdateLantanaRequest()
+    req.session_id = GkillAPI.get_instance().get_session_id()
+    req.lantana = cloned_kyou.value.typed_lantana!!.clone()
+    req.lantana.is_deleted = true
+
+    req.lantana.update_app = "gkill"
+    req.lantana.update_device = gkill_info_res.device
+    req.lantana.update_time = new Date(Date.now())
+    req.lantana.update_user = gkill_info_res.user_id
+
+
+    const res = await GkillAPI.get_instance().update_lantana(req)
+    return res.errors
+}
+
+async function delete_idf_kyou(): Promise<Array<GkillError>> {
+    throw new Error("not implements")
+}
+
+async function delete_git_commit_log(): Promise<Array<GkillError>> {
+    throw new Error("not implements")
+}
+
+async function delete_rekyou(): Promise<Array<GkillError>> {
+    const gkill_info_req = new GetGkillInfoRequest()
+    gkill_info_req.session_id = GkillAPI.get_instance().get_session_id()
+    const gkill_info_res = await GkillAPI.get_instance().get_gkill_info(gkill_info_req)
+
+    const req = new UpdateReKyouRequest()
+    req.session_id = GkillAPI.get_instance().get_session_id()
+    req.rekyou = cloned_kyou.value.typed_rekyou!!.clone()
+    req.rekyou.is_deleted = true
+
+    req.rekyou.update_app = "gkill"
+    req.rekyou.update_device = gkill_info_res.device
+    req.rekyou.update_time = new Date(Date.now())
+    req.rekyou.update_user = gkill_info_res.user_id
+
+    const res = await GkillAPI.get_instance().update_rekyou(req)
+    return res.errors
 }
 </script>
