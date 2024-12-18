a9eda5e21db7a2d0573383193fb0462296783e3c
mt3hr <38493486+mt3hr@users.noreply.github.com>
2024-12-19 01:25:35 +0900

    fix clear query

--- src/pages/views/rykv-query-editor-sidebar-emits.ts
+++ src/pages/views/rykv-query-editor-sidebar-emits.ts
@@ -3,6 +3,7 @@
 import type { FindKyouQuery } from "@/classes/api/find_query/find-kyou-query"
 
 export interface rykvQueryEditorSidebarEmits {
+    (e: 'updated_query_clear', query: FindKyouQuery): void
     (e: 'updated_query', query: FindKyouQuery): void
     (e: 'requested_search'): void
 }
