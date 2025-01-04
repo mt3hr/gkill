1653da59f7097c5f5c7fce437637a6fa41f2f217
mt3hr <38493486+mt3hr@users.noreply.github.com>
2025-01-05 01:20:09 +0900

    PWA

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import App from "./App.vue"
 import router from "./router"
 import vuetify from "./plugins/vuetify"
 import '@mdi/font/css/materialdesignicons.css'
+import { registerSW } from 'virtual:pwa-register'
 
+registerSW()
 const app = createApp(App)
 app.use(router)
 app.use(vuetify)
