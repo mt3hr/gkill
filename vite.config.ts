0e77207167bbba76d80e03af558e1126387b3611
mt3hr <ymt3shr@gmail.com>
2025-01-05 01:20:09 +0900

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,20 @@ export default defineConfig({
       }
     }),
     vueDevTools(),
+    VitePWA({
+      injectRegister: 'auto',
+      manifest: {
+        icons: [{
+          src: "favicon.png",
+          sizes: "144x144",
+          type: "image/png",
+          purpose: "any"
+        }]
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 ** 2, // 5 MB or set to something else
+      },
+    }),
   ],
   resolve: {
     alias: {
