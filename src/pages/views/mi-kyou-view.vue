f6e17f31aadb6f630a0a97f35b29bf9eb5840e36
mt3hr <ymt3shr@gmail.com>
2025-01-10 11:32:03 +0900

    fix style

--- src/pages/views/mi-kyou-view.vue
+++ src/pages/views/mi-kyou-view.vue
@@ -17,7 +17,7 @@
                 </v-card-title>
             </v-col>
         </v-row>
-        <div v-if="kyou.typed_mi && kyou.typed_mi.estimate_start_time">
+        <div v-if="kyou.typed_mi && kyou.typed_mi.estimate_start_time" :style="{ 'padding-top': '30px' }">
             開始日時： <span>{{ format_time(kyou.typed_mi.estimate_start_time) }}</span>
         </div>
         <div v-if="kyou.typed_mi && kyou.typed_mi.estimate_end_time">
