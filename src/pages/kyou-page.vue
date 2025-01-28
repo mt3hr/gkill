c670973cd2f47e77ec410a201d27e48e6db39aa2
mt3hr <38493486+mt3hr@users.noreply.github.com>
2025-01-28 23:04:51 +0900

    Fix ApplicationConfigDialog
    読み込みが終わるまでボタンを無効化する

--- src/pages/kyou-page.vue
+++ src/pages/kyou-page.vue
@@ -19,7 +19,7 @@
             </v-toolbar-title>
             <v-spacer />
             <v-divider vertical />
-            <v-btn icon="mdi-cog" @click="is_show_application_config_dialog = true" />
+            <v-btn icon="mdi-cog" :disabled="!application_config.is_loaded" @click="show_application_config_dialog()" />
         </v-app-bar>
         <v-main class="main">
             <div class="overlay_target">
@@ -37,7 +37,8 @@
             <ApplicationConfigDialog :application_config="application_config" :gkill_api="gkill_api"
                 :app_content_height="app_content_height" :app_content_width="app_content_width"
                 :is_show="is_show_application_config_dialog" @received_errors="write_errors"
-                @received_messages="write_messages" @requested_reload_application_config="load_application_config" />
+                @received_messages="write_messages" @requested_reload_application_config="load_application_config"
+                ref="application_config_dialog" />
         </v-main>
         <div class="alert_container">
             <v-slide-y-transition group>
@@ -63,6 +64,8 @@ import { Kyou } from '@/classes/datas/kyou'
 import { GetGkillNotificationPublicKeyRequest } from '@/classes/api/req_res/get-gkill-notification-public-key-request'
 import { RegisterGkillNotificationRequest } from '@/classes/api/req_res/register-gkill-notification-request'
 import { GetKyouRequest } from '@/classes/api/req_res/get-kyou-request'
+
+const application_config_dialog = ref<InstanceType<typeof ApplicationConfigDialog> | null>(null);
 
 const enable_context_menu = ref(true)
 const enable_dialog = ref(true)
@@ -250,6 +253,9 @@ async function register_gkill_task_notification(): Promise<void> {
     }
 }
 
+function show_application_config_dialog(): void {
+    application_config_dialog.value?.show()
+}
 </script>
 <style lang="css">
 /* 不要なスクロールバーを消す */
