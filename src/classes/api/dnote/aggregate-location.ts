5bc5c253d9888c14e898c8261b199f0e5929ea5d
mt3hr <ymt3shr@gmail.com>
2025-02-21 09:39:55 +0900

    Fix Dnote
    日付をまたぐTimeIsをTrimするように修正

--- src/classes/api/dnote/aggregate-location.ts
+++ src/classes/api/dnote/aggregate-location.ts
@@ -12,7 +12,7 @@ export class AggregateLocation {
     }
 }
 
-export async function aggregate_locations_from_kyous(kyous: Array<Kyou>, abort_controller: AbortController): Promise<Array<AggregateLocation>> {
+export async function aggregate_locations_from_kyous(kyous: Array<Kyou>, abort_controller: AbortController, start_time: Date, end_time: Date): Promise<Array<AggregateLocation>> {
     const aggregate_locations = new Array<AggregateLocation>()
     const aggregate_locations_map = new Map<string, AggregateLocation>()// map[title]aggregate_location
     for (let i = 0; i < kyous.length; i++) {
@@ -34,12 +34,15 @@ export async function aggregate_locations_from_kyous(kyous: Array<Kyou>, abort_c
         let title = ""
         let duration_milli_second = 0
 
+        const now = new Date(Date.now())
         if (kyou.data_type.startsWith("timeis")) {
             if (kyou.typed_timeis) {
                 title = kyou.typed_timeis.title
-                const end_time = kyou.typed_timeis.end_time?.getTime()
-                if ((kyou.typed_timeis.start_time.getTime() < (end_time ? end_time : 0))) {
-                    duration_milli_second = Math.abs(moment.duration(moment(kyou.typed_timeis.start_time).diff(kyou.typed_timeis.end_time)).asMilliseconds())
+                const start_time_trimed = kyou.typed_timeis.start_time.getTime() <= start_time.getTime() ? start_time : kyou.typed_timeis.start_time
+                const end_time_trimed = kyou.typed_timeis.end_time ? (kyou.typed_timeis.end_time.getTime() >= end_time.getTime() ? end_time : kyou.typed_timeis.end_time) : null
+
+                if ((start_time_trimed.getTime() < (end_time_trimed ? end_time_trimed.getTime() : now.getTime()))) {
+                    duration_milli_second = Math.abs(moment.duration(moment(start_time_trimed).diff(moment(end_time_trimed))).asMilliseconds())
                 } else {
                     duration_milli_second = 0
                 }
