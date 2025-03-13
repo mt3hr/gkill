5e35d345346b481fa1cab1d83db357ec6bc12d3d
mt3hr <38493486+mt3hr@users.noreply.github.com>
2025-03-13 12:44:29 +0900

    Fix Lantana Word検索

--- src/app/gkill/dao/sqlite3impl/sqlite3impl_util.go
+++ src/app/gkill/dao/sqlite3impl/sqlite3impl_util.go
@@ -93,116 +93,6 @@ func GenerateFindSQLCommon(query *find.FindQuery, whereCounter *int, onlyLatestD
 
 	// ワードand検索である場合のSQL追記
 	if query.UseWords != nil && *query.UseWords {
-		if query.Words != nil && len(*query.Words) != 0 {
-			if query.WordsAnd != nil && *query.WordsAnd {
-				if *whereCounter != 0 {
-					sql += " AND "
-				}
-				for j, findWordTargetColumnName := range findWordTargetColumns {
-					if j == 0 {
-						sql += " ( "
-					} else {
-						sql += " AND "
-					}
-
-					for i, word := range *query.Words {
-						if i == 0 {
-							sql += " ( "
-						} else {
-							sql += " AND "
-						}
-						if findWordUseLike {
-							sql += fmt.Sprintf("LOWER(%s) LIKE LOWER(?)", findWordTargetColumnName)
-							*queryArgs = append(*queryArgs, "%"+word+"%")
-						} else {
-							sql += fmt.Sprintf("LOWER(%s) = LOWER(?)", findWordTargetColumnName)
-							*queryArgs = append(*queryArgs, word)
-						}
-						if i == len(*query.Words)-1 {
-							sql += " ) "
-						}
-						*whereCounter++
-					}
-
-					if j == len(findWordTargetColumns)-1 {
-						sql += " ) "
-					}
-				}
-			} else {
-				// ワードor検索である場合のSQL追記
-				if *whereCounter != 0 {
-					sql += " AND "
-				}
-				for j, findWordTargetColumnName := range findWordTargetColumns {
-					if j == 0 {
-						sql += " ( "
-					} else {
-						sql += " OR "
-					}
-
-					for i, word := range *query.Words {
-						if i == 0 {
-							sql += " ( "
-						} else {
-							sql += " OR "
-						}
-						if findWordUseLike {
-							sql += fmt.Sprintf("LOWER(%s) LIKE LOWER(?)", findWordTargetColumnName)
-							*queryArgs = append(*queryArgs, "%"+word+"%")
-						} else {
-							sql += fmt.Sprintf("LOWER(%s) = LOWER(?)", findWordTargetColumnName)
-							*queryArgs = append(*queryArgs, word)
-						}
-						if i == len(*query.Words)-1 {
-							sql += " ) "
-						}
-						*whereCounter++
-					}
-
-					if j == len(findWordTargetColumns)-1 {
-						sql += " ) "
-					}
-				}
-			}
-		}
-
-		if query.NotWords != nil && len(*query.NotWords) != 0 {
-			// notワードを除外するSQLを追記
-			if *whereCounter != 0 {
-				sql += " AND "
-			}
-			for j, findWordTargetColumnName := range findWordTargetColumns {
-				if j == 0 {
-					sql += " ( "
-				} else {
-					sql += " AND "
-				}
-
-				for i, notWord := range *query.NotWords {
-					if i == 0 {
-						sql += " ( "
-					} else {
-						sql += " AND "
-					}
-					if findWordUseLike {
-						sql += fmt.Sprintf("LOWER(%s) NOT LIKE LOWER(?)", findWordTargetColumnName)
-						*queryArgs = append(*queryArgs, "%"+notWord+"%")
-					} else {
-						sql += fmt.Sprintf("LOWER(%s) <> LOWER(?)", findWordTargetColumnName)
-						*queryArgs = append(*queryArgs, notWord)
-					}
-					if i == len(*query.NotWords)-1 {
-						sql += " ) "
-					}
-					*whereCounter++
-				}
-
-				if j == len(findWordTargetColumns)-1 {
-					sql += " ) "
-				}
-			}
-		}
-
 		// ワード指定ありで検索対象列がない場合は全部false
 		if ignoreFindWord && len(findWordTargetColumns) == 0 {
 			if *whereCounter != 0 {
@@ -210,6 +100,117 @@ func GenerateFindSQLCommon(query *find.FindQuery, whereCounter *int, onlyLatestD
 			}
 			sql += " 1 = 0 "
 			*whereCounter++
+		} else {
+
+			if query.Words != nil && len(*query.Words) != 0 {
+				if query.WordsAnd != nil && *query.WordsAnd {
+					if *whereCounter != 0 {
+						sql += " AND "
+					}
+					for j, findWordTargetColumnName := range findWordTargetColumns {
+						if j == 0 {
+							sql += " ( "
+						} else {
+							sql += " AND "
+						}
+
+						for i, word := range *query.Words {
+							if i == 0 {
+								sql += " ( "
+							} else {
+								sql += " AND "
+							}
+							if findWordUseLike {
+								sql += fmt.Sprintf("LOWER(%s) LIKE LOWER(?)", findWordTargetColumnName)
+								*queryArgs = append(*queryArgs, "%"+word+"%")
+							} else {
+								sql += fmt.Sprintf("LOWER(%s) = LOWER(?)", findWordTargetColumnName)
+								*queryArgs = append(*queryArgs, word)
+							}
+							if i == len(*query.Words)-1 {
+								sql += " ) "
+							}
+							*whereCounter++
+						}
+
+						if j == len(findWordTargetColumns)-1 {
+							sql += " ) "
+						}
+					}
+				} else {
+					// ワードor検索である場合のSQL追記
+					if *whereCounter != 0 {
+						sql += " AND "
+					}
+					for j, findWordTargetColumnName := range findWordTargetColumns {
+						if j == 0 {
+							sql += " ( "
+						} else {
+							sql += " OR "
+						}
+
+						for i, word := range *query.Words {
+							if i == 0 {
+								sql += " ( "
+							} else {
+								sql += " OR "
+							}
+							if findWordUseLike {
+								sql += fmt.Sprintf("LOWER(%s) LIKE LOWER(?)", findWordTargetColumnName)
+								*queryArgs = append(*queryArgs, "%"+word+"%")
+							} else {
+								sql += fmt.Sprintf("LOWER(%s) = LOWER(?)", findWordTargetColumnName)
+								*queryArgs = append(*queryArgs, word)
+							}
+							if i == len(*query.Words)-1 {
+								sql += " ) "
+							}
+							*whereCounter++
+						}
+
+						if j == len(findWordTargetColumns)-1 {
+							sql += " ) "
+						}
+					}
+				}
+			}
+
+			if query.NotWords != nil && len(*query.NotWords) != 0 {
+				// notワードを除外するSQLを追記
+				if *whereCounter != 0 {
+					sql += " AND "
+				}
+				for j, findWordTargetColumnName := range findWordTargetColumns {
+					if j == 0 {
+						sql += " ( "
+					} else {
+						sql += " AND "
+					}
+
+					for i, notWord := range *query.NotWords {
+						if i == 0 {
+							sql += " ( "
+						} else {
+							sql += " AND "
+						}
+						if findWordUseLike {
+							sql += fmt.Sprintf("LOWER(%s) NOT LIKE LOWER(?)", findWordTargetColumnName)
+							*queryArgs = append(*queryArgs, "%"+notWord+"%")
+						} else {
+							sql += fmt.Sprintf("LOWER(%s) <> LOWER(?)", findWordTargetColumnName)
+							*queryArgs = append(*queryArgs, notWord)
+						}
+						if i == len(*query.NotWords)-1 {
+							sql += " ) "
+						}
+						*whereCounter++
+					}
+
+					if j == len(findWordTargetColumns)-1 {
+						sql += " ) "
+					}
+				}
+			}
 		}
 	}
 	if *whereCounter == 0 {
