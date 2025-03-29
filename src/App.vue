36b85d11411360355a71116ddad449f0c6a9a8a0
mt3hr <38493486+mt3hr@users.noreply.github.com>
2025-03-29 19:08:04 +0900

    Fix モバイル端末でのサイドバーちらつき

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
 import { type Ref, ref } from 'vue';
 import { RouterView } from 'vue-router'
 import { VLocaleProvider } from 'vuetify/components';
+import { useTheme } from 'vuetify'
+import { GkillAPI } from './classes/api/gkill-api';
+
+const theme = useTheme()
+const use_dark_theme = GkillAPI.get_gkill_api().get_use_dark_theme()
+if (use_dark_theme) {
+  theme.global.name.value = 'gkill_dark_theme'
+} else {
+  theme.global.name.value = 'gkill_theme'
+}
+
 const locale: Ref<string> = ref(window.navigator.language)
 </script>
 
