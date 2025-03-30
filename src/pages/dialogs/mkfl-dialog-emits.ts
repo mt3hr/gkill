bc400b552c52601d3c44c3e719f801a06bea35d1
mt3hr <38493486+mt3hr@users.noreply.github.com>
2025-03-30 17:04:56 +0900

    Implements MKFL Dialog

---
+++ src/pages/dialogs/mkfl-dialog-emits.ts
@@ -0,0 +1,15 @@
+'use strict'
+
+import type { GkillError } from "@/classes/api/gkill-error"
+import type { GkillMessage } from "@/classes/api/gkill-message"
+import type { Kyou } from "@/classes/datas/kyou"
+
+export interface MKFLDialogEmits {
+    (e: 'received_messages', message: Array<GkillMessage>): void
+    (e: 'received_errors', errors: Array<GkillError>): void
+    (e: 'registered_kyou', kyou: Kyou): void
+    (e: 'updated_kyou', kyou: Kyou): void
+    (e: 'deleted_kyou', kyou: Kyou): void
+    (e: 'requested_close_dialog'): void
+    (e: 'saved_kyou_by_kftl', last_added_request_time: Date): void
+}
---
+++ src/pages/dialogs/mkfl-dialog.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-dialog :width="'fit-content'" v-model="is_show_dialog" class="mkfl_dialog">
+        <MKFLView :app_content_height="app_content_height" :app_content_width="app_content_width.valueOf() - 50"
+            :application_config="application_config" :gkill_api="gkill_api"
+            @received_messages="(messages: Array<GkillMessage>) => emits('received_messages', messages)"
+            @received_errors="(errors: Array<GkillError>) => emits('received_errors', errors)" />
+    </v-dialog>
+</template>
+<script lang="ts" setup>
+import { type Ref, ref } from 'vue'
+import type { GkillError } from '@/classes/api/gkill-error'
+import type { GkillMessage } from '@/classes/api/gkill-message'
+import type { MKFLDialogEmits } from './mkfl-dialog-emits'
+import type { MKFLDialogProps } from './mkfl-dialog-props'
+import MKFLView from '../views/mkfl-view.vue'
+
+defineProps<MKFLDialogProps>()
+const emits = defineEmits<MKFLDialogEmits>()
+defineExpose({ show, hide })
+
+const is_show_dialog: Ref<boolean> = ref(false)
+
+async function show(): Promise<void> {
+    is_show_dialog.value = true
+}
+async function hide(): Promise<void> {
+    is_show_dialog.value = false
+}
+</script>
+<style lang="css">
+.mkfl_dialog.v-dialog .v-card {
+    overflow-y: hidden;
+}
+
+.mkfl_dialog.v-dialog .v-card::-webkit-scrollbar {
+    width: 0px;
+}
+</style>
