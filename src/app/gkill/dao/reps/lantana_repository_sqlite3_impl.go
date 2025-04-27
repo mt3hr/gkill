96de058cd7e25713c54af54f64e76636c80bca99
mt3hr <ymt3shr@gmail.com>
2025-04-27 19:44:22 +0900

    Fix？PlaingTimeIsが読み込まれない時があるサーバ不具合 DB接続数制限 解除

--- src/app/gkill/dao/reps/lantana_repository_sqlite3_impl.go
+++ src/app/gkill/dao/reps/lantana_repository_sqlite3_impl.go
@@ -27,8 +27,6 @@ func NewLantanaRepositorySQLite3Impl(ctx context.Context, filename string) (Lant
 		err = fmt.Errorf("error at open database %s: %w", filename, err)
 		return nil, err
 	}
-	db.SetMaxOpenConns(5)
-	db.SetMaxIdleConns(5)
 
 	sql := `
 CREATE TABLE IF NOT EXISTS "LANTANA" (
--- src/app/gkill/dao/reps/notification_repository_sqlite3_impl.go
+++ src/app/gkill/dao/reps/notification_repository_sqlite3_impl.go
@@ -27,8 +27,6 @@ func NewNotificationRepositorySQLite3Impl(ctx context.Context, filename string) 
 		err = fmt.Errorf("error at open database %s: %w", filename, err)
 		return nil, err
 	}
-	db.SetMaxOpenConns(5)
-	db.SetMaxIdleConns(5)
 
 	sql := `
 CREATE TABLE IF NOT EXISTS "NOTIFICATION" (
