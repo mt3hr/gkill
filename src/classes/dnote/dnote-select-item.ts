317d6388e8086450e5d8c04bfe58c2a920a26aa7
mt3hr <ymt3shr@gmail.com>
2025-05-02 09:17:55 +0900

    Implements Dnoteユーザ定義集計項目編集画面

---
+++ src/classes/dnote/dnote-select-item.ts
@@ -0,0 +1,1 @@
+export default interface DnoteSelectItem { label: string, value: string }
---
+++ src/pages/views/edit-dnote-predicate-group.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-card class="predicate-group" variant="outlined">
+        <v-select v-model="group!.logic" :items="['AND', 'OR', 'NOT']" :label="$t('DNOTE_JOIN_LOGIC_TITLE')" />
+
+        <div v-for="(predicate, index) in group!.predicates" :key="index" class="predicate-entry">
+            <PredicateGroup v-if="is_group(predicate)" v-model="group!.predicates[index] as PredicateGroupType"
+                class="my-2" @remove="remove_predicate(index)" />
+            <PredicateCard v-else v-model="group!.predicates[index] as Predicate" class="my-2"
+                @remove="remove_predicate(index)" />
+        </div>
+        <v-btn @click="add_predicate" :color="'primary'" class="ma-1">{{ $t('DNOTE_ADD_PREDICATE_TITLE') }}</v-btn>
+        <v-btn @click="add_group" :color="'primary'" class="ma-1">{{ $t('DNOTE_ADD_GROUP_TITLE') }}</v-btn>
+        <v-btn @click="emits('remove')" :color="'secondary'" class="ma-1"> {{ $t('DNOTE_DELETE_GROUP_TITLE') }}</v-btn>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+const emits = defineEmits(['remove'])
+import { defineModel } from 'vue'
+import PredicateCard from './edit-dnote-card.vue'
+import PredicateGroup from './edit-dnote-predicate-group.vue'
+import type Predicate from '../../classes/dnote/predicate'
+import type PredicateGroupType from '../../classes/dnote/predicate-group-type'
+import { i18n } from '@/i18n'
+
+const group = defineModel<PredicateGroupType>()!
+
+function is_group(p: Predicate | PredicateGroupType): p is PredicateGroupType {
+    return 'logic' in p && Array.isArray(p.predicates)
+}
+
+function add_predicate() {
+    group.value!.predicates.push({ type: i18n.global.t("DNOTE_DATA_TYPE_PREFIX_PREDICATE"), value: "" })
+}
+
+function add_group() {
+    group.value!.predicates.push({ logic: 'AND', predicates: [] })
+}
+
+function remove_predicate(index: number) {
+    group.value!.predicates.splice(index, 1)
+}
+</script>
