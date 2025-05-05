e9578cff01b71f28c3a34ee67e2b09e4a80801a2
mt3hr <38493486+mt3hr@users.noreply.github.com>
2025-05-06 07:32:40 +0900

    Update Style Dnote集計

---
+++ src/classes/dnote/pulldown-menu/predicate-menu-items.ts
@@ -0,0 +1,29 @@
+import { i18n } from "@/i18n";
+
+export default [
+    { label: i18n.global.t("DNOTE_DATA_TYPE_PREFIX_PREDICATE"), value: "DataTypePrefixPredicate" },
+    { label: i18n.global.t("DNOTE_TAG_EQUAL_PREDICATE"), value: "TagEqualPredicate" },
+    { label: i18n.global.t("DNOTE_KMEMO_CONTENT_CONTAINS_PREDICATE"), value: "KmemoContentContainsPredicate" },
+    { label: i18n.global.t("DNOTE_KMEMO_CONTENT_EQUAL_PREDICATE"), value: "KmemoContentEqualPredicate" },
+    { label: i18n.global.t("DNOTE_TIMEIS_TITLE_CONTAINS_PREDICATE"), value: "TimeIsTitleContainsPredicate" },
+    { label: i18n.global.t("DNOTE_TIMEIS_TITLE_EQUAL_PREDICATE"), value: "TimeIsTitleEqualPredicate" },
+    { label: i18n.global.t("DNOTE_MI_TITLE_CONTAINS_PREDICATE"), value: "MiTitleEqualPredicate" },
+    { label: i18n.global.t("DNOTE_MI_TITLE_EQUAL_PREDICATE"), value: "MiTitleEqualPredicate" },
+    { label: i18n.global.t("DNOTE_NLOG_TITLE_CONTAINS_PREDICATE"), value: "NlogTitleContainsPredicate" },
+    { label: i18n.global.t("DNOTE_NLOG_TITLE_EQUAL_PREDICATE"), value: "NlogTitleEqualPredicate" },
+    { label: i18n.global.t("DNOTE_NLOG_AMOUNT_GREATER_THAN_PREDICATE"), value: "NlogAmountGreaterThanPredicate" },
+    { label: i18n.global.t("DNOTE_NLOG_AMOUNT_LESS_THAN_PREDICATE"), value: "NlogAmountLessThanPredicate" },
+    { label: i18n.global.t("DNOTE_NLOG_SHOP_CONTAINS_PREDICATE"), value: "NlogShopContainsPredicate" },
+    { label: i18n.global.t("DNOTE_NLOG_SHOP_EQUAL_PREDICATE"), value: "NlogShopEqualPredicate" },
+    { label: i18n.global.t("DNOTE_LANTANA_MOOD_GREATER_THAN_PREDICATE"), value: "LantanaMoodGreaterThanPredicate" },
+    { label: i18n.global.t("DNOTE_LANTANA_MOOD_LESS_THAN_PREDICATE"), value: "LantanaMoodLessThanPredicate" },
+    { label: i18n.global.t("DNOTE_LANTANA_MOOD_EQUAL_PREDICATE"), value: "LantanaMoodEqualPredicate" },
+    { label: i18n.global.t("DNOTE_RELATED_TIME_WEEK_PREDICATE"), value: "RelatedTimeWeekPredicate" },
+    { label: i18n.global.t("DNOTE_GIT_COMMIT_LOG_CODE_GREATER_THAN_PREDICATE"), value: "GitCommitLogCodeGreaterThanPredicate" },
+    { label: i18n.global.t("DNOTE_GIT_COMMIT_LOG_CODE_LESS_THAN_PREDICATE"), value: "GitCommitLogCodeLessThanPredicate" },
+    { label: i18n.global.t("DNOTE_GIT_COMMIT_LOG_ADDITION_CODE_GREATER_THAN_PREDICATE"), value: "GitCommitLogCodeAdditionGreaterThanPredicate" },
+    { label: i18n.global.t("DNOTE_GIT_COMMIT_LOG_ADDITION_CODE_LESS_THAN_PREDICATE"), value: "GitCommitLogCodeDeletionLessThanPredicate" },
+    { label: i18n.global.t("DNOTE_GIT_COMMIT_LOG_DELETION_CODE_GREATER_THAN_PREDICATE"), value: "GitCommitLogCodeAdditionGreaterThanPredicate" },
+    { label: i18n.global.t("DNOTE_GIT_COMMIT_LOG_DELETION_CODE_LESS_THAN_PREDICATE"), value: "GitCommitLogCodeDeletionLessThanPredicate" },
+
+]
--- src/pages/views/edit-dnote-card.vue
+++ src/pages/views/edit-dnote-card.vue
@@ -17,37 +17,10 @@ const emits = defineEmits(['remove'])
 import { ref, type Ref } from 'vue'
 import { defineModel } from 'vue'
 import type Predicate from '../../classes/dnote/predicate'
-import { i18n } from '@/i18n'
+import predicate_menu_items from '@/classes/dnote/pulldown-menu/predicate-menu-items'
 import type DnoteSelectItem from '../../classes/dnote/dnote-select-item'
 
 const model_value = defineModel<Predicate>()
 
-const predicate_types: Ref<Array<DnoteSelectItem>> = ref([
-    { label: i18n.global.t("DNOTE_DATA_TYPE_PREFIX_PREDICATE"), value: "DataTypePrefixPredicate" },
-    { label: i18n.global.t("DNOTE_GIT_COMMIT_LOG_CODE_GREATER_THAN_PREDICATE"), value: "GitCommitLogCodeGreaterThanPredicate" },
-    { label: i18n.global.t("DNOTE_GIT_COMMIT_LOG_CODE_LESS_THAN_PREDICATE"), value: "GitCommitLogCodeLessThanPredicate" },
-    { label: i18n.global.t("DNOTE_GIT_COMMIT_LOG_ADDITION_CODE_GREATER_THAN_PREDICATE"), value: "GitCommitLogCodeAdditionGreaterThanPredicate" },
-    { label: i18n.global.t("DNOTE_GIT_COMMIT_LOG_ADDITION_CODE_LESS_THAN_PREDICATE"), value: "GitCommitLogCodeDeletionLessThanPredicate" },
-    { label: i18n.global.t("DNOTE_GIT_COMMIT_LOG_DELETION_CODE_GREATER_THAN_PREDICATE"), value: "GitCommitLogCodeAdditionGreaterThanPredicate" },
-    { label: i18n.global.t("DNOTE_GIT_COMMIT_LOG_DELETION_CODE_LESS_THAN_PREDICATE"), value: "GitCommitLogCodeDeletionLessThanPredicate" },
-    { label: i18n.global.t("DNOTE_KMEMO_CONTENT_CONTAINS_PREDICATE"), value: "KmemoContentContainsPredicate" },
-    { label: i18n.global.t("DNOTE_KMEMO_CONTENT_EQUAL_PREDICATE"), value: "KmemoContentEqualPredicate" },
-    { label: i18n.global.t("DNOTE_LANTANA_MOOD_EQUAL_PREDICATE"), value: "LantanaMoodEqualPredicate" },
-    { label: i18n.global.t("DNOTE_LANTANA_MOOD_GREATER_THAN_PREDICATE"), value: "LantanaMoodGreaterThanPredicate" },
-    { label: i18n.global.t("DNOTE_LANTANA_MOOD_LESS_THAN_PREDICATE"), value: "LantanaMoodLessThanPredicate" },
-    { label: i18n.global.t("DNOTE_MI_TITLE_CONTAINS_PREDICATE"), value: "MiTitleEqualPredicate" },
-    { label: i18n.global.t("DNOTE_MI_TITLE_EQUAL_PREDICATE"), value: "MiTitleEqualPredicate" },
-    { label: i18n.global.t("DNOTE_NLOG_AMOUNT_GREATER_THAN_PREDICATE"), value: "NlogAmountGreaterThanPredicate" },
-    { label: i18n.global.t("DNOTE_NLOG_AMOUNT_LESS_THAN_PREDICATE"), value: "NlogAmountLessThanPredicate" },
-    { label: i18n.global.t("DNOTE_NLOG_SHOP_CONTAINS_PREDICATE"), value: "NlogShopContainsPredicate" },
-    { label: i18n.global.t("DNOTE_NLOG_SHOP_EQUAL_PREDICATE"), value: "NlogShopEqualPredicate" },
-    { label: i18n.global.t("DNOTE_NLOG_TITLE_CONTAINS_PREDICATE"), value: "NlogTitleContainsPredicate" },
-    { label: i18n.global.t("DNOTE_NLOG_TITLE_EQUAL_PREDICATE"), value: "NlogTitleEqualPredicate" },
-    { label: i18n.global.t("DNOTE_RELATED_TIME_WEEK_PREDICATE"), value: "RelatedTimeWeekPredicate" },
-    { label: i18n.global.t("DNOTE_TAG_EQUAL_PREDICATE"), value: "TagEqualPredicate" },
-    { label: i18n.global.t("DNOTE_TIMEIS_TITLE_CONTAINS_PREDICATE"), value: "TimeIsTitleContainsPredicate" },
-    { label: i18n.global.t("DNOTE_TIMEIS_TITLE_EQUAL_PREDICATE"), value: "TimeIsTitleEqualPredicate" },
-])
-
-const predicate_type = ref(model_value.value?.type)
+const predicate_types: Ref<Array<DnoteSelectItem>> = ref(predicate_menu_items)
 </script>
