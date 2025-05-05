a40d2fd81b543013df233a92f49b41528e0df96b
mt3hr <38493486+mt3hr@users.noreply.github.com>
2025-05-06 04:42:55 +0900

    Clean

--- src/pages/views/aggregate-location-view-props.ts
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import type { AggregateLocation } from "@/classes/api/dnote/aggregate-location"
-import type { GkillPropsBase } from "./gkill-props-base"
-
-export interface AggregateLocationViewProps extends GkillPropsBase {
-    last_added_tag: string
-    aggregate_location: AggregateLocation
-}
