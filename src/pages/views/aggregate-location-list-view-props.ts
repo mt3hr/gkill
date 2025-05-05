742a1893a0a870de9d4c38f1e4f261d86486fe49
mt3hr <ymt3shr@gmail.com>
2025-05-06 04:42:55 +0900

    Clean

--- src/pages/views/aggregate-location-list-view-props.ts
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import type { AggregateLocation } from "@/classes/api/dnote/aggregate-location"
-import type { GkillPropsBase } from "./gkill-props-base"
-
-export interface AggregateLocationListViewProps extends GkillPropsBase {
-    last_added_tag: string
-    aggregate_locations: Array<AggregateLocation>
-}
