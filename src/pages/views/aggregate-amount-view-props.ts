8db4ba2e46412ea37095dccbb2260296797129fd
mt3hr <38493486+mt3hr@users.noreply.github.com>
2025-05-06 04:42:55 +0900

    Clean

--- src/pages/views/aggregate-amount-view-props.ts
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import type { AggregateAmount } from "@/classes/api/dnote/aggregate-amount"
-import type { GkillPropsBase } from "./gkill-props-base"
-
-export interface AggregateAmountViewProps extends GkillPropsBase {
-    last_added_tag: string
-    aggregate_amount: AggregateAmount
-}
--- src/pages/views/aggregate-people-view-props.ts
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import type { AggregatePeople } from "@/classes/api/dnote/aggregate-people"
-import type { GkillPropsBase } from "./gkill-props-base"
-
-export interface AggregatePeopleViewProps extends GkillPropsBase {
-    last_added_tag: string
-    aggregate_people: AggregatePeople
-}
