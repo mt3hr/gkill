8a3777988a2b89ccba1027de1cb48d811e5437d4
mt3hr <38493486+mt3hr@users.noreply.github.com>
2025-05-16 23:08:16 +0900

    Fix Mi,TimeIs 履歴では古い情報も表示できるように修正

--- src/pages/views/kyou-view.vue
+++ src/pages/views/kyou-view.vue
@@ -408,14 +408,14 @@ watch(() => props.kyou, async () => {
     cloned_kyou.value.abort_controller = new AbortController()
     cloned_kyou.value = props.kyou.clone()
     if (props.force_show_latest_kyou_info) {
-        await cloned_kyou.value.reload();//最新を読み込むためにReload
+        await cloned_kyou.value.reload(true);//最新を読み込むためにReload
     }
     (() => load_attached_infos())(); //非同期で実行してほしい
 });
 
 (async () => {
     if (props.force_show_latest_kyou_info) {
-        await cloned_kyou.value.reload();//最新を読み込むためにReload
+        await cloned_kyou.value.reload(true);//最新を読み込むためにReload
     }
     load_attached_infos()
 })(); //非同期で実行してほしい
