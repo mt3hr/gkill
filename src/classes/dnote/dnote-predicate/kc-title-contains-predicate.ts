35ef8a5db10c4cc747c27ddb04b6c8b6479418c9
mt3hr <ymt3shr@gmail.com>
2025-07-03 11:02:11 +0900

    Fix Ryuu KC周り

---
+++ src/classes/dnote/dnote-predicate/kc-title-contains-predicate.ts
@@ -0,0 +1,29 @@
+import type { Kyou } from "@/classes/datas/kyou";
+import type DnotePredicate from "../dnote-predicate";
+import PredicateDictonary from "../serialize/dnote-predicate-dictionary";
+
+export default class KCTitleContainsPredicate implements DnotePredicate {
+    private kc_title_contains_target: string
+    constructor(kc_title_contains_target: string) {
+        this.kc_title_contains_target = kc_title_contains_target
+    }
+    static from_json(json: any): DnotePredicate {
+        const kc_title_contains_target = json.value as string
+        return new KCTitleContainsPredicate(kc_title_contains_target)
+    }
+    async is_match(loaded_kyou: Kyou): Promise<boolean> {
+        const kc_title = loaded_kyou.typed_kc?.title
+        if (kc_title) {
+            if (kc_title?.includes(this.kc_title_contains_target)) {
+                return true
+            }
+        }
+        return false
+    }
+    predicate_struct_to_json(): any {
+        return {
+            type: "KCTitleContainsPredicate",
+            value: this.kc_title_contains_target,
+        }
+    }
+}
---
+++ src/classes/dnote/dnote-predicate/kc-title-equal-predicate.ts
@@ -0,0 +1,29 @@
+import type { Kyou } from "@/classes/datas/kyou";
+import type DnotePredicate from "../dnote-predicate";
+import PredicateDictonary from "../serialize/dnote-predicate-dictionary";
+
+export default class KCTitleEqualPredicate implements DnotePredicate {
+    private kc_title_equal_target: string
+    constructor(kc_title_equal_target: string) {
+        this.kc_title_equal_target = kc_title_equal_target
+    }
+    static from_json(json: any): DnotePredicate {
+        const kc_title_equal_target = json.value as string
+        return new KCTitleEqualPredicate(kc_title_equal_target)
+    }
+    async is_match(loaded_kyou: Kyou): Promise<boolean> {
+        const kc_title = loaded_kyou.typed_kc?.title
+        if (kc_title) {
+            if (kc_title === this.kc_title_equal_target) {
+                return true
+            }
+        }
+        return false
+    }
+    predicate_struct_to_json(): any {
+        return {
+            type: "KCTitleEqualPredicate",
+            value: this.kc_title_equal_target,
+        }
+    }
+}
