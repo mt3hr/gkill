360adc02f1d50b4ed6e2d8126c1e91f3f6425e85
mt3hr <ymt3shr@gmail.com>
2025-07-23 03:20:30 +0900

    Fix Cache タグやテキストのキャッシュが更新されない不具合修正

--- src/app/gkill/dao/account_state/latest_data_repository_address.go
+++ src/app/gkill/dao/account_state/latest_data_repository_address.go
@@ -13,13 +13,14 @@ import (
 type LatestDataRepositoryAddress struct {
 	IsDeleted                              bool      `json:"is_deleted"`
 	TargetID                               string    `json:"target_id"`
+	TargetIDInData                         *string   `json:"target_id_in_data"`
 	LatestDataRepositoryName               string    `json:"latest_data_repository_name"`
 	DataUpdateTime                         time.Time `json:"data_update_time"`
 	LatestDataRepositoryAddressUpdatedTime time.Time `json:"latest_data_repository_address_updated_time"`
 }
 
 func (l *LatestDataRepositoryAddress) ContentHash() string {
-	seed := []byte(fmt.Sprintf("%t_%s_%s_%s", l.IsDeleted, l.TargetID, l.LatestDataRepositoryName, l.DataUpdateTime.Format(sqlite3impl.TimeLayout)))
+	seed := []byte(fmt.Sprintf("%t_%s_%v_%s_%s", l.IsDeleted, l.TargetID, l.TargetIDInData, l.LatestDataRepositoryName, l.DataUpdateTime.Format(sqlite3impl.TimeLayout)))
 	sum := sha256.Sum256(seed)
 	return hex.EncodeToString(sum[:])
 }
