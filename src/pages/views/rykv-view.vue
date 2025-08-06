e5a59c9999e8151e1cdd8e8ff3fb744e4b3c4361
mt3hr <38493486+mt3hr@users.noreply.github.com>
2025-08-07 08:02:42 +0900

    Update +ボタン長押しでKFTLダイアログを表示

--- src/pages/views/rykv-view.vue
+++ src/pages/views/rykv-view.vue
@@ -348,7 +348,8 @@
                 class="position-fixed">
                 <v-menu :style="add_kyou_menu_style" transition="slide-x-transition">
                     <template v-slot:activator="{ props }">
-                        <v-btn color="white" icon="mdi-plus" variant="text" v-bind="props" />
+                        <v-btn color="white" icon="mdi-plus" v-long-press="() => show_kftl_dialog()" variant="text"
+                            v-bind="props" />
                     </template>
                     <v-list>
                         <v-list-item @click="show_kftl_dialog()">
