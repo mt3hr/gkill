b26686628c3c946bec4751d7e38260d0f5c67ae7
mt3hr <ymt3shr@gmail.com>
2025-11-24 03:47:50 +0900

    Fix 履歴のMiとTimeIsの状態が編集できてしまう不具合修正

--- src/pages/dialogs/kyou-dialog-props.ts
+++ src/pages/dialogs/kyou-dialog-props.ts
@@ -3,5 +3,6 @@
 import type { KyouViewPropsBase } from "../views/kyou-view-props-base"
 
 export interface KyouDialogProps extends KyouViewPropsBase {
-   
+    show_timeis_plaing_end_button: boolean
+    is_readonly_mi_check: boolean
 }
