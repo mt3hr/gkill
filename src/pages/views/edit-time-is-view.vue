f7cc21d41ea650eb87f24f9f24e4094bfe3087e4
mt3hr <ymt3shr@gmail.com>
2025-11-26 05:32:50 +0900

    Fix 記録の日時編集で、同一の分を入力したときに閉じない不具合

--- src/pages/views/edit-time-is-view.vue
+++ src/pages/views/edit-time-is-view.vue
@@ -39,7 +39,7 @@
                                         v-bind="props" />
                                 </template>
                                 <v-time-picker v-model="timeis_start_time_string" format="24hr"
-                                    @update:model-value="show_start_time_menu = false" />
+                                    @update:minute="show_start_time_menu = false" />
                             </v-menu>
                         </td>
                     </tr>
@@ -87,7 +87,7 @@
                                         v-bind="props" />
                                 </template>
                                 <v-time-picker v-model="timeis_end_time_string" format="24hr"
-                                    @update:model-value="show_end_time_menu = false" />
+                                    @update:minute="show_end_time_menu = false" />
                             </v-menu>
                         </td>
                     </tr>
