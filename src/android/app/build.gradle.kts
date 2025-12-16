ac5da6c4a469520faa4dda9327389fa9331a08a3
mt3hr <ymt3shr@gmail.com>
2025-12-16 12:56:28 +0900

    v1.0.10

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 28
         versionCode = 1
-        versionName = "1.0.10-dev"
+        versionName = "1.0.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
