027f19660c8747ec8385898782a58d4c9e9bc227
mt3hr <ymt3shr@gmail.com>
2025-12-30 20:43:45 +0900

    Update CreateAccount MenuTitle

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 28
         versionCode = 1
-        versionName = "1.0.11"
+        versionName = "1.0.12-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
