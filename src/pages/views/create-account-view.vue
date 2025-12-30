f095ab36e389994ab0ef56d5512d0b0ab1034d75
mt3hr <ymt3shr@gmail.com>
2025-12-30 20:43:45 +0900

    Update CreateAccount MenuTitle

--- src/pages/views/create-account-view.vue
+++ src/pages/views/create-account-view.vue
@@ -8,7 +8,7 @@
             </v-row>
         </v-card-title>
         <v-text-field v-model="new_user_id" :label="i18n.global.t('USER_ID_TITLE')" />
-        <v-checkbox v-model="do_initialize" :label="i18n.global.t('DO_INITIALIZE_ADD_ACCOUNT_MESSAGE')" />
+        <v-checkbox v-model="do_not_initialize" :label="i18n.global.t('DO_INITIALIZE_ADD_ACCOUNT_MESSAGE')" />
         <v-card-action>
             <v-row class="pa-0 ma-0">
                 <v-col cols="auto" class="pa-0 ma-0">
@@ -33,7 +33,7 @@ import { AddAccountRequest } from '@/classes/api/req_res/add-account-request';
 const props = defineProps<CreateAccountViewProps>()
 const emits = defineEmits<CreateAccountViewEmits>()
 const new_user_id: Ref<string> = ref("")
-const do_initialize: Ref<boolean> = ref(false)
+const do_not_initialize: Ref<boolean> = ref(false)
 
 async function create_account(): Promise<void> {
     const req = new AddAccountRequest()
@@ -41,7 +41,7 @@ async function create_account(): Promise<void> {
     req.account_info.is_admin = false
     req.account_info.password_reset_token = null
     req.account_info.user_id = new_user_id.value
-    req.do_initialize = do_initialize.value
+    req.do_initialize = !do_not_initialize.value
 
     const res = await props.gkill_api.add_account(req)
     if (res.errors && res.errors.length !== 0) {
