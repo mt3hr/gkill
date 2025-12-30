909fe5db02a91bb42a871fdc961ff197aa09682e
mt3hr <ymt3shr@gmail.com>
2025-12-30 13:11:53 +0900

    Fix gkill_sample_data launch bat

--- resources/gkill_sample_data/LAUNCH_GKILL_SAMPLE_DATA.bat
+++ resources/gkill_sample_data/LAUNCH_GKILL_SAMPLE_DATA.bat
@@ -1,17 +1,20 @@
-@echo 【gkillサンプルデータ】
-@echo 1.下記のアドレスにアクセス
-@echo 　http://localhost:8888/
-@echo;
-
-@echo 2.以下アカウントでログイン
-@echo 　ユーザID   gkill_sample_data
-@echo 　パスワード sample
-@echo;
-
-@echo 3.この黒窓をクリックして「CTRL+C」で終了
-@echo;
-
 @echo off
+pushd "%~dp0"
 
-start http://localhost:8888/
+echo 【 gkillサンプルデータ 】
+echo 1.下記のアドレスにアクセス
+echo    http://localhost:8888/
+echo.
+
+echo 2.以下アカウントでログイン
+echo    ユーザID: gkill_sample_data
+echo    パスワード: sample
+echo.
+
+echo 3.この黒窓をクリックして CTRL+C で終了
+echo.
+
+start "" "http://localhost:8888/"
 gkill_server --gkill_home_dir .
+
+popd
