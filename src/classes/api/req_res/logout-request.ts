9bf8023965047bad783d03f660073e4cce410318
mt3hr <ymt3shr@gmail.com>
2026-01-02 12:00:38 +0900

    Implements 完全なログアウト

--- src/classes/api/req_res/logout-request.ts
+++ src/classes/api/req_res/logout-request.ts
@@ -4,8 +4,11 @@ import { GkillAPIRequest } from '../gkill-api-request'
 
 export class LogoutRequest extends GkillAPIRequest {
 
+    close_database: boolean
+
     constructor() {
         super()
+        this.close_database = false
     }
 
 }
