58f27d6f886a67ab6ddd7c3db27cea2606618075
mt3hr <38493486+mt3hr@users.noreply.github.com>
2026-01-04 15:04:18 +0900

    Fix 記録動作が遅い件
    ApplicationConfigをキャッシュするように修正、画面起動時のみ再取得する

--- src/pages/kyou-page.vue
+++ src/pages/kyou-page.vue
@@ -122,7 +122,7 @@ async function load_kyou(): Promise<void> {
 
 async function load_application_config(): Promise<void> {
     const req = new GetApplicationConfigRequest()
-
+    req.force_reget = true
     return gkill_api.value.get_application_config(req)
         .then(res => {
             if (res.errors && res.errors.length != 0) {
