9b8c72e57d00bdf1366fe31e0d2aa5a74e8e792d
mt3hr <38493486+mt3hr@users.noreply.github.com>
2026-01-16 13:07:50 +0900

    Implements Dnoteドラッグアンドドロップ並び替え

--- src/pages/views/dnote-item-props.ts
+++ src/pages/views/dnote-item-props.ts
@@ -1,9 +1,9 @@
 import type { GkillAPI } from "../../classes/api/gkill-api"
 import type { ApplicationConfig } from "../../classes/datas/config/application-config"
-import type DnoteItem from "../../classes/dnote/dnote-item"
 
 export default interface DnoteItemProps {
     gkill_api: GkillAPI
     application_config: ApplicationConfig
     editable: boolean
+    dnd_list_index: number
 }
--- src/pages/views/dnote-item-view-emits.ts
+++ src/pages/views/dnote-item-view-emits.ts
@@ -5,4 +5,5 @@ export default interface DnoteItemViewEmits extends KyouViewEmits {
     (e: 'requested_delete_dnote_item', dnote_item_id: string): void
     (e: 'requested_update_dnote_item', dnote_item: DnoteItem): void
     (e: 'finish_a_aggregate_task'): void
+    (e: "requested_move_dnote_item", srcId: string, srcListIndex: number, targetId: string | null, targetListIndex: number, dropType: "up" | "down"): void
 }
