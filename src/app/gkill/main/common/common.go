d6877701efde68cd45a957c3e250b6a37b0ee96e
mt3hr <ymt3shr@gmail.com>
2026-01-28 03:18:10 +0900

    DisablePprof

--- src/app/gkill/main/common/common.go
+++ src/app/gkill/main/common/common.go
@@ -126,9 +126,11 @@ func init() {
 	}
 	fixTimezone()
 
-	go func() {
-		http.ListenAndServe("localhost:6060", nil) // pprof用
-	}()
+	/*
+		go func() {
+			http.ListenAndServe("localhost:6060", nil) // pprof用
+		}()
+	*/
 
 	IDFCmd.PersistentFlags().StringArrayVarP(&gkill_options.IDFIgnore, "ignore", "i", gkill_options.IDFIgnore, "ignore files")
 }
