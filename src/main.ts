fab8137e33d94ce95efb5d6653eeb3307369656d
mt3hr <ymt3shr@gmail.com>
2026-01-28 03:17:45 +0900

    AbortError握りつぶし考慮追加

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,16 @@ import '@mdi/font/css/materialdesignicons.css'
 import { registerSW } from 'virtual:pwa-register'
 import { i18n } from "./i18n"
 import { vLongPress } from "./classes/long-press"
+import { isAbortError } from "@/utils/isAbortError"
 
 registerSW()
+
+/* abortは握りつぶす */
+window.addEventListener("unhandledrejection", (event) => {
+    if (isAbortError(event.reason)) {
+        event.preventDefault()
+    }
+})
 
 const app = createApp(App)
 app.directive('long-press', vLongPress)
