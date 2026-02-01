e804093ec8ba4036fbbf1cb26b875f9b51179138
mt3hr <38493486+mt3hr@users.noreply.github.com>
2026-02-02 07:41:52 +0900

    Update GkillMessage見た目 主張控えめ

--- src/pages/shared-rykv-page.vue
+++ src/pages/shared-rykv-page.vue
@@ -10,7 +10,7 @@
                 <v-tooltip :text="(message.is_error ? 'エラーコード' : 'メッセージコード') + ':' + message.code"
                     v-for="message in messages" :key="message.id">
                     <template v-slot:activator="{ props }">
-                        <v-alert v-bind="props" :color="message.is_error ? 'error' : 'info'"
+                        <v-alert v-bind="props" :color="message.is_error ? 'error' : undefined"
                             :closable="message.closable" @click:close="close_message(message.id)">
                             {{ message.message }}
                         </v-alert>
@@ -148,7 +148,12 @@ body::-webkit-scrollbar {
 }
 
 /* メッセージ、エラーメッセージ */
+.alert_container > div {
+    width: fit-content;
+}
+
 .alert_container {
+    justify-items: end;
     position: fixed;
     top: 60px;
     right: 10px;
