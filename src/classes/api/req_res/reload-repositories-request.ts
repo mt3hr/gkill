77b4b644121350f5c47713f50f3d0754e77c952f
mt3hr <ymt3shr@gmail.com>
2026-02-02 05:12:38 +0900

    Update ReloadRepositories
    長押しでサムネイルキャッシュもクリア

--- src/classes/api/req_res/reload-repositories-request.ts
+++ src/classes/api/req_res/reload-repositories-request.ts
@@ -3,6 +3,12 @@
 import { GkillAPIRequest } from '../gkill-api-request'
 
 export class ReloadRepositoriesRequest extends GkillAPIRequest {
+    clear_thumb_cache: boolean
+
+    constructor() {
+        super()
+        this.clear_thumb_cache = false
+    }
 }
 
 
