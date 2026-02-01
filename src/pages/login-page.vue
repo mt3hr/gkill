a4ba0e4b6fdd1cb3981ec1eb9cf06eaf07a31cb1
mt3hr <ymt3shr@gmail.com>
2026-02-02 07:41:52 +0900

    Update GkillMessage見た目 主張控えめ

--- src/pages/login-page.vue
+++ src/pages/login-page.vue
@@ -15,7 +15,7 @@
                 <v-tooltip :text="(message.is_error ? 'エラーコード' : 'メッセージコード') + ':' + message.code"
                     v-for="message in messages" :key="message.id">
                     <template v-slot:activator="{ props }">
-                        <v-alert v-bind="props" :color="message.is_error ? 'error' : 'info'"
+                        <v-alert v-bind="props" :color="message.is_error ? 'error' : undefined"
                             :closable="message.closable" @click:close="close_message(message.id)">
                             {{ message.message }}
                         </v-alert>
@@ -161,7 +161,12 @@ html {
     margin-right: 15px;
 }
 
+.alert_container > div {
+    width: fit-content;
+}
+
 .alert_container {
+    justify-items: end;
     position: fixed;
     top: 60px;
     right: 10px;
