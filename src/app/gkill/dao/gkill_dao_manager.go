f7fe8bc1d14b6b367f0c8ac365ca36b793d4732a
mt3hr <ymt3shr@gmail.com>
2026-02-04 11:19:41 +0900

    Update IDF 高速化

--- src/app/gkill/dao/gkill_dao_manager.go
+++ src/app/gkill/dao/gkill_dao_manager.go
@@ -33,7 +33,6 @@ type GkillDAOManager struct {
 	ConfigDAOs *ConfigDAOs
 
 	router    *mux.Router
-	autoIDF   *bool
 	IDFIgnore []string
 
 	enableOutputLogs bool
@@ -55,11 +54,8 @@ func NewGkillDAOManager() (*GkillDAOManager, error) {
 		return nil, err
 	}
 
-	autoIDF := true
-
 	ctx := context.Background()
 	gkillDAOManager := &GkillDAOManager{
-		autoIDF:                  &autoIDF,
 		router:                   &mux.Router{},
 		IDFIgnore:                gkill_options.IDFIgnore,
 		fileRepWatchCacheUpdater: fileRepWatchCacheUpdater,
@@ -697,7 +693,7 @@ func (g *GkillDAOManager) GetRepositories(userID string, device string) (*reps.G
 					}
 
 					idDBFilename := filepath.Join(parentDir, "gkill_id.db")
-					idfKyouRep, err := reps.NewIDFDirRep(ctx, filename, idDBFilename, g.router, &autoIDF, &g.IDFIgnore, repositories)
+					idfKyouRep, err := reps.NewIDFDirRep(ctx, filename, idDBFilename, rep.UseToWrite, g.router, autoIDF, &g.IDFIgnore, repositories)
 					if err != nil {
 						return nil, err
 					}
@@ -1017,7 +1013,6 @@ func (g *GkillDAOManager) Close() error {
 
 	g.ConfigDAOs = nil
 	g.router = nil
-	g.autoIDF = nil
 	g.IDFIgnore = []string{}
 
 	if g.enableOutputLogs {
