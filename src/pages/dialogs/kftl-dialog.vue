7d39a8ea05acfffa9fc667fccfce248a9bd33c5a
mt3hr <ymt3shr@gmail.com>
2026-02-21 00:53:11 +0900

    Update 透過チェックボックスの文字色調整

--- src/pages/dialogs/kftl-dialog.vue
+++ src/pages/dialogs/kftl-dialog.vue
@@ -8,7 +8,7 @@
         @touchstart="ui.onHeaderPointerDown">
         <div class="gkill-floating-dialog__title"></div>
         <div class="gkill-floating-dialog__spacer"></div>
-        <v-checkbox v-model="ui.isTransparent.value" dark  size="small" variant="flat"
+  <v-checkbox v-model="ui.isTransparent.value" color="white"    size="small" variant="flat"
           :label="i18n.global.t('TRANSPARENT_TITLE')" hide-details />
         <v-btn size="small" class="rounded-sm mx-auto" icon @click.prevent="hide" hide-details :color="'primary'"
           variant="flat">
