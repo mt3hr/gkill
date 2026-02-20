ba72c68ca53dd009fc9340d7f813b3a268f108c2
mt3hr <38493486+mt3hr@users.noreply.github.com>
2026-02-20 14:22:29 +0900

    Update Dialog Style

--- src/pages/dialogs/kyou-list-view-dialog.vue
+++ src/pages/dialogs/kyou-list-view-dialog.vue
@@ -4,11 +4,11 @@
 
         <div :ref="ui.containerRef" :style="ui.fixedStyle.value" class="gkill-floating-dialog"
             :class="ui.isTransparent.value ? 'is-transparent' : ''">
-            <div class="gkill-floating-dialog__header" @mousedown="ui.onHeaderPointerDown"
+            <div class="gkill-floating-dialog__header pa-0 ma-0" @mousedown="ui.onHeaderPointerDown"
                 @touchstart="ui.onHeaderPointerDown">
                 <div class="gkill-floating-dialog__title"></div>
                 <div class="gkill-floating-dialog__spacer"></div>
-                <v-checkbox v-model="ui.isTransparent.value" size="small" variant="flat" 
+                <v-checkbox v-model="ui.isTransparent.value" dark  size="small" variant="flat" 
                     :label="i18n.global.t('TRANSPARENT_TITLE')" hide-details />
                 <v-btn size="small" class="rounded-sm mx-auto" icon @click.prevent="hide" hide-details :color="'primary'" variant="flat"> 
           <v-icon>mdi-close</v-icon>
