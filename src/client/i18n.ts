e19a6866215871bc5f05378783d119efd66218e3
mt3hr <38493486+mt3hr@users.noreply.github.com>
2026-03-05 04:10:36 +0900

    move locales directory

--- src/client/i18n.ts
+++ src/client/i18n.ts
@@ -1,11 +1,11 @@
 import { createI18n } from "vue-i18n"
-import ja from './locales/ja.json'
-import en from './locales/en.json'
-import zh from './locales/zh.json'
-import ko from './locales/ko.json'
-import es from './locales/es.json'
-import fr from './locales/fr.json'
-import de from './locales/de.json'
+import ja from '../locales/ja.json'
+import en from '../locales/en.json'
+import zh from '../locales/zh.json'
+import ko from '../locales/ko.json'
+import es from '../locales/es.json'
+import fr from '../locales/fr.json'
+import de from '../locales/de.json'
 
 export const i18n = createI18n({
     legacy: true,
