0aee11af4eb3f0e19d508dcb56d19189aa942ff5
mt3hr <38493486+mt3hr@users.noreply.github.com>
2026-03-13 13:30:48 +0900

    Ryuu, Dnote RepTypeの条件をプルダウン化

---
+++ src/client/classes/dnote/pulldown-menu/rep-type-menu-items.ts
@@ -0,0 +1,12 @@
+import { i18n } from "@/i18n";
+
+export default [
+    { label: i18n.global.t("REP_TYPE_KMEMO"), value: "kmemo" },
+    { label: i18n.global.t("REP_TYPE_URLOG"), value: "urlog" },
+    { label: i18n.global.t("REP_TYPE_NLOG"), value: "nlog" },
+    { label: i18n.global.t("REP_TYPE_KC"), value: "kc" },
+    { label: i18n.global.t("REP_TYPE_TIMEIS"), value: "timeis" },
+    { label: i18n.global.t("REP_TYPE_MI"), value: "mi" },
+    { label: i18n.global.t("REP_TYPE_LANTANA"), value: "lantana" },
+    { label: i18n.global.t("REP_TYPE_REKYOU"), value: "rekyou" },
+]
