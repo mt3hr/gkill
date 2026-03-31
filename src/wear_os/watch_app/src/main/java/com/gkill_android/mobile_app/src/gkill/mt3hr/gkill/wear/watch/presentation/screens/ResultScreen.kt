d84fc32ec3202d5b8cfb67ae60a6b36d79fcf827
mt3hr <38493486+mt3hr@users.noreply.github.com>
2026-03-31 17:20:07 +0900

    Update wear_os 記録したら2秒後にもdる考慮追加

--- src/wear_os/watch_app/src/main/java/com/gkill_android/mobile_app/src/gkill/mt3hr/gkill/wear/watch/presentation/screens/ResultScreen.kt
+++ src/wear_os/watch_app/src/main/java/com/gkill_android/mobile_app/src/gkill/mt3hr/gkill/wear/watch/presentation/screens/ResultScreen.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.Text
+import kotlinx.coroutines.delay
 
 /**
  * Shows the result of the KFTL submission.
@@ -24,6 +26,12 @@ fun ResultScreen(
     errorMessage: String = "",
     onDismiss: () -> Unit
 ) {
+    if (success) {
+        LaunchedEffect(Unit) {
+            delay(2000L)
+            onDismiss()
+        }
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
