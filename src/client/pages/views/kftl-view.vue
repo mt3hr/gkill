f595464fb8e68397b44e2e7695fc965e4a6c92e5
mt3hr <38493486+mt3hr@users.noreply.github.com>
2026-08-01 13:31:03 +0900

    Implements KFTL 新しいタグが追加されたら確認を求めるように

--- src/client/pages/views/kftl-view.vue
+++ src/client/pages/views/kftl-view.vue
@@ -42,6 +42,54 @@
             v-on="errorMessageRelayHandlers"
             @clicked_template_element_leaf="paste_template" ref="kftl_template_dialog" />
     </v-card>
+    <Teleport to="body" v-if="show_confirm_unknown_tag_dialog">
+        <div class="gkill-float-scrim" :class="confirm_dialog_ui.isTransparent.value ? 'is-transparent' : ''" />
+        <div :ref="confirm_dialog_ui.containerRef" :style="confirm_dialog_ui.fixedStyle.value"
+            class="gkill-floating-dialog"
+            :class="confirm_dialog_ui.isTransparent.value ? 'is-transparent' : ''">
+            <div class="gkill-floating-dialog__header pa-0 ma-0"
+                @mousedown="confirm_dialog_ui.onHeaderPointerDown"
+                @touchstart="confirm_dialog_ui.onHeaderPointerDown">
+                <div class="gkill-floating-dialog__title"></div>
+                <div class="gkill-floating-dialog__spacer"></div>
+                <v-checkbox v-model="confirm_dialog_ui.isTransparent.value" color="white" size="small" variant="flat"
+                    :label="i18n.global.t('TRANSPARENT_TITLE')" hide-details />
+                <v-btn size="small" class="rounded-sm mx-auto" icon @click.prevent="cancel_submit()" hide-details
+                    :color="'primary'" variant="flat">
+                    <v-icon>mdi-close</v-icon>
+                </v-btn>
+            </div>
+            <div class="gkill-floating-dialog__body">
+                <v-card variant="flat" class="pa-2">
+                    <v-card-title>
+                        <v-row class="pa-0 ma-0">
+                            <v-col cols="auto" class="pa-0 ma-0">
+                                <span>{{ i18n.global.t("CONFIRM_UNKNOWN_TAG_TITLE") }}</span>
+                            </v-col>
+                        </v-row>
+                    </v-card-title>
+                    <v-card-text>
+                        {{ i18n.global.t("CONFIRM_UNKNOWN_TAG_MESSAGE") }}
+                        <v-list density="compact">
+                            <v-list-item v-for="tag in unknown_tags" :key="tag">
+                                <v-list-item-title>{{ tag }}</v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                    </v-card-text>
+                    <v-row class="pa-0 ma-0 flex-row-reverse gkill-dialog-actions">
+                        <v-col cols="auto" class="pa-0 ma-0">
+                            <v-btn dark color="primary" @click="confirm_submit()">{{ i18n.global.t("SAVE_TITLE")
+                                }}</v-btn>
+                        </v-col>
+                        <v-col cols="auto" class="pa-0 ma-0">
+                            <v-btn @click="cancel_submit()">{{ i18n.global.t("CANCEL_TITLE") }}</v-btn>
+                        </v-col>
+                        <v-spacer />
+                    </v-row>
+                </v-card>
+            </div>
+        </div>
+    </Teleport>
 </template>
 
 <script setup lang="ts">
@@ -67,6 +115,11 @@ const {
     line_label_styles,
     is_requested_submit,
     title_height,
+    show_confirm_unknown_tag_dialog,
+    unknown_tags,
+
+    // Dialog UI
+    confirm_dialog_ui,
 
     // Computed
     text_area_width_px,
@@ -78,6 +131,8 @@ const {
 
     // Business logic
     submit,
+    cancel_submit,
+    confirm_submit,
     show_kftl_template_dialog,
     paste_template,
     focus_kftl_text_area,
